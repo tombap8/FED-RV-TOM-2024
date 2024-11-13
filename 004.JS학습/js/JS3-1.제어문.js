8b64c78378d43fed39d071c225044e8850a040ac
Tom B. Parker <tombap8@hotmail.com>
2024-11-13 12:19:53 +0900

    Update JS3-1.제어문.js

--- 004.JS학습/js/JS3-1.제어문.js
+++ 004.JS학습/js/JS3-1.제어문.js
@@ -37,38 +37,45 @@ function showJumsu(){
     // 4. 점수에 따른 분기하여 결과 출력 //////
     // if/else if를 사용하면 구간설정 불필요!
 
-    // 평가텍스트 변수
+    // ### 결과담는 변수 셋팅 ### //
+    // (1) 평가텍스트 변수
     var expText = "";
-    // 평가결과 글자색 변수
+    // (2) 평가결과 글자색 변수
     var expColor = "";
-    // 칭찬스티커 위치값 변수
+    // (3) 칭찬스티커 위치값 변수
     var jshowPos = "";
 
     // 4-1. 90점 이상 : 매우잘함
     if(score >= 90){
         expText = "매우잘함";
+        expColor = "blue";
     }
     // 4-2. 80점 이상 : 잘함
     else if(score >= 80){
         expText = "잘함";
+        expColor = "green";
     }
     // 4-3. 70점 이상 : 보통
     else if(score >= 70){
         expText = "보통";
+        expColor = "purple";
     }
     // 4-4. 60점 이상 : 노력요함
     else if(score >= 60){
         expText = "노력요함";
+        expColor = "orange";
     }
     // 4-5. 60점 미만 : 재시험
     else{
         expText = "재시험";
+        expColor = "red";
     }
 
     // 5. 화면출력 변경하기 //////
     // 5-1. 텍스트 변경하기
     jexp.innerText = "평가결과는 " + expText;
     // 5-2. 글자색 변경하기
+    jexp.style.color = expColor;
     // 5-3. 칭찬스티커 변경하기
 
 
