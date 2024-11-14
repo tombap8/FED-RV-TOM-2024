72a2759ef3ff0e1b2c3ceb9af853adae04277e66
Tom B. Parker <tombap8@hotmail.com>
2024-11-14 15:27:34 +0900

    Update JS3-1.제어문.js

--- 004.JS학습/js/JS3-1.제어문.js
+++ 004.JS학습/js/JS3-1.제어문.js
@@ -12,98 +12,130 @@ console.log("나, 제어문 테스트!");
         3. 점수에 따른 칭찬스티커 보이기
 **************************************/
 // 이름있는 함수를 선언적 함수라고함!
-function showJumsu(){
-    // 1. 함수호출확인!
-    // console.log("내 평가를 알려줘!");
+function showJumsu() {
+  // 1. 함수호출확인!
+  // console.log("내 평가를 알려줘!");
 
-    // 2. 대상선정 ////////
-    // 2-1. 점수입력요소 : input#jumsu
-    var jumsu = document.querySelector('#jumsu');
+  // 2. 대상선정 ////////
+  // 2-1. 점수입력요소 : input#jumsu
+  var jumsu =
+    document.querySelector("#jumsu");
 
-    // 2-2. 출력요소 : .jexp
-    var jexp = document.querySelector('.jexp');
+  // 2-2. 출력요소 : .jexp
+  var jexp =
+    document.querySelector(".jexp");
 
-    // 2-3. 칭찬스티커요소 : .jshow
-    var jshow = document.querySelector('.jshow');
+  // 2-3. 칭찬스티커요소 : .jshow
+  var jshow =
+    document.querySelector(".jshow");
 
-    // console.log("선택요소:",jumsu, jexp, jshow);
+  // console.log("선택요소:",jumsu, jexp, jshow);
 
+  // 3. 점수입력값 읽어오기 : jumsu변수의 input요소값
+  var score = jumsu.value;
+  // value속성 - 입력된 input값을 읽어오는 속성
+  // console.log("입력된값:",score);
 
-    // 3. 점수입력값 읽어오기 : jumsu변수의 input요소값
-    var score = jumsu.value;
-    // value속성 - 입력된 input값을 읽어오는 속성
-    // console.log("입력된값:",score);
-
-    /**************************************** 
+  /**************************************** 
         [ if문을 이용하여 데이터 필터링하기!]
         (1) 문자이면 돌려보내기
         (2) 숫자범위가 아니면 돌려보내기
         -> 공통 : 메시지 찍기 + 초기화
     ****************************************/
-   // 3.5-1. 문자이면 돌려보내기
-   // 숫자가 아니니? is Not a Number?
-   // JS 내장함수 isNaN(보낼값) -> 결과는?
-   // 숫자가 아니면 true, 숫자이면 false
-   if(isNaN(score)){
-        console.log("숫자가 아니구만~! 나가!!!");
-        // 함수를 나가는 키워드는? return
-        return;
-   } /// if ///
+  // 3.5-1. 문자이면 돌려보내기 ////////
+  // 숫자가 아니니? is Not a Number?
+  // JS 내장함수 isNaN(보낼값) -> 결과는?
+  // 숫자가 아니면 true, 숫자이면 false
+  if (isNaN(score)) {
+    console.log(
+      "숫자가 아니구만~! 나가!!!"
+    );
 
+    // 초기화하기 ////////////
+    // (1) 텍스트 변경하기
+    jexp.innerText =
+      "숫자만 입력하세요! 제발~!";
+    // (2) 글자색 변경하기
+    jexp.style.color = "black";
+    // (3) 칭찬스티커 변경하기
+    jshow.style.backgroundPosition =
+      "-50% 0%";
 
+    // 함수를 나가는 키워드는? return
+    return;
+  } /// if ///
+  // 3.5-2. 숫자범위가 아니면 돌려보내기 /////
+  // 조건범위 : 0보다 작거나 100보다 크면 true
+  else if (score < 0 || score > 100) {
+    console.log(
+      "점수범위가 아니구만! 나가!!!"
+    );
 
+    // 초기화하기 ////////////
+    // (1) 텍스트 변경하기
+    jexp.innerText =
+      "0~100 사이숫자만 입력하세요! 제발~!";
+    // (2) 글자색 변경하기
+    jexp.style.color = "black";
+    // (3) 칭찬스티커 변경하기
+    jshow.style.backgroundPosition =
+      "-50% 0%";
 
-    // 4. 점수에 따른 분기하여 결과 출력 //////
-    // if/else if를 사용하면 구간설정 불필요!
+    // 함수를 나가는 키워드는? return
+    return;
+  } /// else if ///
 
-    // ### 결과담는 변수 셋팅 ### //
-    // (1) 평가텍스트 변수
-    var expText = "";
-    // (2) 평가결과 글자색 변수
-    var expColor = "";
-    // (3) 칭찬스티커 위치값 변수
-    var jshowPos = "";
+  // 4. 점수에 따른 분기하여 결과 출력 //////
+  // if/else if를 사용하면 구간설정 불필요!
 
-    // 4-1. 90점 이상 : 매우잘함
-    if(score >= 90){
-        expText = "매우잘함";
-        expColor = "blue";
-        jshowPos = "100% 50%";
-    }
-    // 4-2. 80점 이상 : 잘함
-    else if(score >= 80){
-        expText = "잘함";
-        expColor = "green";
-        jshowPos = "0% 100%";
-    }
-    // 4-3. 70점 이상 : 보통
-    else if(score >= 70){
-        expText = "보통";
-        expColor = "purple";
-        jshowPos = "50% 50%";
-    }
-    // 4-4. 60점 이상 : 노력요함
-    else if(score >= 60){
-        expText = "노력요함";
-        expColor = "orange";
-        jshowPos = "100% 100%";
-    }
-    // 4-5. 60점 미만 : 재시험
-    else{
-        expText = "재시험";
-        expColor = "red";
-        jshowPos = "100% 0%";
-    }
+  // ### 결과담는 변수 셋팅 ### //
+  // (1) 평가텍스트 변수
+  var expText = "";
+  // (2) 평가결과 글자색 변수
+  var expColor = "";
+  // (3) 칭찬스티커 위치값 변수
+  var jshowPos = "";
 
-    // 5. 화면출력 변경하기 //////
-    // 5-1. 텍스트 변경하기
-    jexp.innerText = "평가결과는 " + expText;
-    // 5-2. 글자색 변경하기
-    jexp.style.color = expColor;
-    // 5-3. 칭찬스티커 변경하기
-    jshow.style.backgroundPosition = jshowPos;
+  // 4-1. 90점 이상 : 매우잘함
+  if (score >= 90) {
+    expText = "매우잘함";
+    expColor = "blue";
+    jshowPos = "100% 50%";
+  }
+  // 4-2. 80점 이상 : 잘함
+  else if (score >= 80) {
+    expText = "잘함";
+    expColor = "green";
+    jshowPos = "0% 100%";
+  }
+  // 4-3. 70점 이상 : 보통
+  else if (score >= 70) {
+    expText = "보통";
+    expColor = "purple";
+    jshowPos = "50% 50%";
+  }
+  // 4-4. 60점 이상 : 노력요함
+  else if (score >= 60) {
+    expText = "노력요함";
+    expColor = "orange";
+    jshowPos = "100% 100%";
+  }
+  // 4-5. 60점 미만 : 재시험
+  else {
+    expText = "재시험";
+    expColor = "red";
+    jshowPos = "100% 0%";
+  }
 
-
+  // 5. 화면출력 변경하기 //////
+  // 5-1. 텍스트 변경하기
+  jexp.innerText =
+    "평가결과는 " + expText;
+  // 5-2. 글자색 변경하기
+  jexp.style.color = expColor;
+  // 5-3. 칭찬스티커 변경하기
+  jshow.style.backgroundPosition =
+    jshowPos;
 } /////////// showJumsu함수 /////////////
 
 /*************************************** 
@@ -154,70 +186,78 @@ function showJumsu(){
         if(aa>10) my = "ㅎㅎㅎ";
     ***************************************/
 
-    // if문 튜닝~~~
-    if(true){
-        console.log("조건통과1!!!");
-    }
+// if문 튜닝~~~
+if (true) {
+  console.log("조건통과1!!!");
+}
 
-    if(false){
-        console.log("조건통과2!!!");
-    }
-    else{
-        console.log("불통과!!!");
-    }
+if (false) {
+  console.log("조건통과2!!!");
+} else {
+  console.log("불통과!!!");
+}
 
-    // 변수를 사용한 불린값 체크
-    var condition;
+// 변수를 사용한 불린값 체크
+var condition;
 
-    console.log("할당안된변수값:",condition);
+console.log(
+  "할당안된변수값:",
+  condition
+);
 
-    // if문 처리결과 찍기 함수 //////////
-    var showResult = function(txt){ // txt - 전달변수
-        // 호출확인 + 검사종류 타이틀 찍기
-        console.log("♥"+txt+"♥");
-        // if문 테스트하기!
-        if(condition){
-            console.log(condition,"if문 통과!");
-        }
-        else{
-            console.log(condition,": false처리!");
-        }
-    }; //////////// showResult함수 /////////////
+// if문 처리결과 찍기 함수 //////////
+var showResult = function (txt) {
+  // txt - 전달변수
+  // 호출확인 + 검사종류 타이틀 찍기
+  console.log("♥" + txt + "♥");
+  // if문 테스트하기!
+  if (condition) {
+    console.log(
+      condition,
+      "if문 통과!"
+    );
+  } else {
+    console.log(
+      condition,
+      ": false처리!"
+    );
+  }
+}; //////////// showResult함수 /////////////
 
-    // 테스트 1 : undefined
-    // 선언후 할당되지 않은 변수값은 undefined임!
-    showResult("테스트 1 : undefined");
+// 테스트 1 : undefined
+// 선언후 할당되지 않은 변수값은 undefined임!
+showResult("테스트 1 : undefined");
 
-    // 테스트 2 : 숫자
-    // -> 0 은 false, 이외의 숫자는 true
-    // true는 1, false는 0과 1:1매칭됨!
-    condition = 1;
-    showResult("테스트 2 : 숫자");
-    condition = 0;
-    showResult("테스트 2 : 숫자");
-    condition = -545665156456415;
-    showResult("테스트 2 : 숫자");
-    
-    // 테스트 3 : null
-    // -> null은 '빈값'을 의미하는 데이터
-    condition = null;
-    showResult("테스트 3 : null");
-    
-    // 테스트 4 : 비교연산자
-    // -> 비교연산자는 결과로 true/false를 리턴함
-    condition = 34 < 1;
-    showResult("테스트 4 : 비교연산자 (34 < 1)");
-    condition = 34 > 1;
-    showResult("테스트 4 : 비교연산자 (34 > 1)");
-    
-    // 테스트 5 : 선언된적이 없는 변수
-    // -> 아예 에러가 발생함!
-    // condition = mymymy;
-    // showResult("테스트 5 : 선언된적이 없는 변수");
+// 테스트 2 : 숫자
+// -> 0 은 false, 이외의 숫자는 true
+// true는 1, false는 0과 1:1매칭됨!
+condition = 1;
+showResult("테스트 2 : 숫자");
+condition = 0;
+showResult("테스트 2 : 숫자");
+condition = -545665156456415;
+showResult("테스트 2 : 숫자");
 
-    // 상단 콘솔출력 지우기
-    console.clear();
+// 테스트 3 : null
+// -> null은 '빈값'을 의미하는 데이터
+condition = null;
+showResult("테스트 3 : null");
 
+// 테스트 4 : 비교연산자
+// -> 비교연산자는 결과로 true/false를 리턴함
+condition = 34 < 1;
+showResult(
+  "테스트 4 : 비교연산자 (34 < 1)"
+);
+condition = 34 > 1;
+showResult(
+  "테스트 4 : 비교연산자 (34 > 1)"
+);
 
+// 테스트 5 : 선언된적이 없는 변수
+// -> 아예 에러가 발생함!
+// condition = mymymy;
+// showResult("테스트 5 : 선언된적이 없는 변수");
 
-
+// 상단 콘솔출력 지우기
+console.clear();
