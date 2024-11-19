4ad7125bf270434e48512255b549d24cf449b4bd
Tom B. Parker <tombap8@hotmail.com>
2024-11-19 11:16:37 +0900

    Update JS3-4.for문연습1.js

--- 004.JS학습/js/JS3-4.for문연습1.js
+++ 004.JS학습/js/JS3-4.for문연습1.js
@@ -36,12 +36,61 @@
 
 ***********************************************/
 
-
 // 1. 대상선정 //////////////////////
 // 1-1. 이벤트 대상 : .mini
-let mini = document.querySelectorAll('.mini');
+let mini =
+  document.querySelectorAll(".mini");
 // 1-2. 변경 대상 : .mini-space
-let miniSpace = document.querySelector('.mini-space');
+let miniSpace = document.querySelector(
+  ".mini-space"
+);
 
-console.log("선택요소:",mini,miniSpace);
+// console.log("선택요소:",mini,mini.length+"개",miniSpace);
 
+// 2. 이벤트 속성 셋팅하기 ///////////////
+// 이벤트 대상에게 click이벤트와 함수를 연결한다!
+
+// mini.item(0).onclick = insertMini;
+
+// mini에 담긴 html컬렉션의 개수는? 변수.length
+// 개수만큼 for문을 돌아서 클릭 이벤트 셋팅을 한다!
+// for(시;한;증){코드}
+
+// 전체 mini개수
+const miniCnt = mini.length;
+
+for (let i = 0; i < miniCnt; i++) {
+  mini[i].onclick = insertMini;
+} /// for ///
+
+// 3. 함수 만들기 ///////////////////////////
+function insertMini() {
+  // this -> 호출한 요소 자신!
+
+  // 1. 호출한 요소의 개수속성 읽어오기 : data-num
+  let num = this.getAttribute('data-num');
+  
+  // [속성값 읽어오기 2가지]
+  // 1) 선택요소.속성명
+  // 2) 선택요소.getAttribute(속성명)
+
+  // [속성값 셋팅법 2가지]
+  // 1) 선택요소.속성명 = 값
+  // 예) document.querySelector('img').src = 경로
+  // 2) 선택요소.setAttribute(속성명,값)
+  // 예) document.querySelector('img')
+  //    .setAttribute("src",경로)
+
+
+  // 2. 함수호출확인
+  console.log("미니언즈 넣어바바~~!",num);
+
+  // 3. 변경대상요소(miniSpace)에 이미지 넣기 
+  // += 대입연산자 : 기존값에 새로운값을 더함!
+  for(let i=0; i<num; i++) {
+      miniSpace.innerHTML += `
+        <img src="./images/Minions.png" alt="미니언즈">
+      `;
+  } //// for ////  
+
+} ////////// insertMini 함수 ////////////////
