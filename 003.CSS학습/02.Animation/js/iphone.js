96c6ffbbb53af763a82db5c20f0fb52dfadb4265
Tom B. Parker <tombap8@hotmail.com>
2024-11-19 15:01:31 +0900

    ss

--- 003.CSS학습/02.Animation/js/iphone.js
+++ 003.CSS학습/02.Animation/js/iphone.js
@@ -6,5 +6,23 @@
 // (CSS에서 마우스 포인터도 이미지로 바꿔준다!)
 
 // 1. 이벤트 대상 : .evt-bx li
-const target = document.querySelectorAll('.evt-bx li');
-console.log("대상요소:", target);
+const target =
+  document.querySelectorAll(
+    ".evt-bx li"
+  );
+
+// 2. 이벤트 설정하기 ////////////
+// 요소개수 ////
+const eleCnt = target.length;
+
+console.log(
+  "대상요소:",
+  target,
+  eleCnt
+);
+
+// 개수 만큼 for문 돌려서 이벤트 설정하기 /////
+// for(시;한;증){코드}
+for (let i = 0; i < eleCnt; i++) {
+  console.log("나돌아?", i);
+} //// for ////
