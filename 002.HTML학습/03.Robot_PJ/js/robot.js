7f713d09f2c068eac385a634344d30de5cc1e648
Tom B. Parker <tombap8@hotmail.com>
2024-11-20 11:05:39 +0900

    Update robot.js

--- 002.HTML학습/03.Robot_PJ/js/robot.js
+++ 002.HTML학습/03.Robot_PJ/js/robot.js
@@ -1,9 +1,8 @@
 // 로봇 PJ JS - robot.js
 
-
 // 로딩구역 만들기 /////////////
 // 대상 : window (최상위 JS 객체)
-// 이벤트 등록 메서드 : 
+// 이벤트 등록 메서드 :
 // addEventListener(이벤트,함수)
 // -> 이벤트와 함수를 연결시키는 메서드
 // -> 이벤트 : JS에 등록된 이벤트명을 사용
@@ -11,12 +10,51 @@
 // 함수는 외부함수 또는 직접 익명함수를 사용함!
 
 /////// 로딩구역 ////////////////////////
-// 로딩관련 이벤트는 2가지!
+// [ 로딩관련 이벤트는 2가지! ]
 // 1) load : 모든 컨텐츠 내용이 로딩된 후 실행
 // -> 이미지나 모든 시간이 걸리는 컨텐츠도
 // 모두 로딩될때까지 기다림!
-window.addEventListener("load",
-function(){
-    console.log("로딩완료!");
+// 2) DOMContentLoaded : 태그만 다그리면 실행
 
-}); ///////// 로드구역 //////////////
+window.addEventListener(
+  "DOMContentLoaded",
+  loadFn
+);
+///////// 로드구역 //////////////
+
+/// 로딩실행 함수 //////////////////
+function loadFn() {
+  console.log("로딩완료!");
+
+  // 0. 요구사항 분석:
+  // 1) TV버튼 클릭시 기능을 수행함
+  // 2) 확대/축소 버튼클릭 : 확대/축소함
+  // 3) 채널버튼 클릭 : 유튜브 동영상 변경
+  // - 확대/축소시 트랜지션 적용하기
+
+  // 1. 대상선정
+  // 1-1. 이벤트 대상 : TV버튼 - .btns button
+  const btns =
+    document.querySelectorAll(
+      ".btns button"
+    );
+
+  console.log("대상:", btns);
+
+  // 2. 이벤트 설정하기 : addEventListener사용!
+  // 버튼개수변수
+  const btnCnt = btns.length;
+  // for문으로 돌면서 이벤트 한꺼번에 설정하기
+//   for(시;한;증){코드}
+for(let i=0; i<btnCnt; i++){
+    btns[i].addEventListener('click',controlTV);
+} ///// for /////
+
+  // 3.  함수만들기
+  function controlTV(){
+    // 1. 함수호출확인!
+    console.log("티비틀어!");
+
+  } ///// controlTV 함수 //////////////
+
+} //////////// loadFn 함수 //////////////
