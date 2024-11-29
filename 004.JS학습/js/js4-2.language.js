663ddb271335fefee5b8a1518a045d393fe329f7
Tom B. Parker <tombap8@hotmail.com>
2024-11-29 12:08:10 +0900

    Update js4-2.language.js

--- 004.JS학습/js/js4-2.language.js
+++ 004.JS학습/js/js4-2.language.js
@@ -15,9 +15,24 @@ import langData from './data_lang.json' with {type:'json'};
 
 console.log("다국어 셋팅객체:",langData);
 
+// 나의 함수 불러오기 /////
+import myFn from './my_function.js';
+// js확장자 생략은 SPA 개발환경에서만 가능함!
+
+console.log('나의함수객체:',myFn);
+
+
 /// 1. 다국어 요구사항 
 // - 언어박스에서 언어를 변경하면 코드에 맞게 다국어 데이터를
 // 제이슨 파일에서 읽어와 본 페이지의 해당 요소를 변경한다!
+
+// 2. 대상선정
+// 2-1. 이벤트 대상 : .sel (드롭다운 선택박스)
+// -> 이벤트 종류 : change 이벤트
+const selBox = myFn.qs('.sel');
+
+
+console.log('대상요소:',selBox);
 
 
 
