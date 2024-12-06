7092a7f0b058b7184914977a892c1b44cb44073c
Tom B. Parker <tombap8@hotmail.com>
2024-12-06 13:10:01 +0900

    Update js6-1.date_math.js

--- 004.JS학습/js/js6-1.date_math.js
+++ 004.JS학습/js/js6-1.date_math.js
@@ -123,30 +123,30 @@ function showTime() {
 
 // 난수 발생시키기
 let rdm = Math.random();
-console.log('난수:',rdm);
+console.log("난수:", rdm);
 
 // 1~7 사이 난수 발생하기
-// 방법 : 
+// 방법 :
 // 1. 난수에 발생할 최대수를 곱한다!
 // 결과: 0 ~ 최대수-1
 rdm = rdm * 7;
-console.log('난수*7:',rdm);
+console.log("난수*7:", rdm);
 // 2. 0부터 생기므로 올림처리한다!
 // 올림 메서드는 ceil()
 // 만약 0부터 필요하면 내림처리함 -> floor()
-console.log('난수*7 올림:',Math.ceil(rdm));
-console.log('난수*7 내림:',Math.floor(rdm));
+console.log("난수*7 올림:", Math.ceil(rdm));
+console.log("난수*7 내림:", Math.floor(rdm));
 
 // 중간 난수는 어떻게하나?
 // 4~12 사이 난수 만들기
 // 우선 두 수를 빼고 (12-4=8)
 // 최대수는 8일까?
 // 올림을 하므로 1을 더해서 9를 만듬
-rdm = Math.random()*8;
-console.log('난수*9 올림:',Math.ceil(rdm));
+rdm = Math.random() * 8;
+console.log("난수*9 올림:", Math.ceil(rdm));
 // 시작수 - 1 = 4 - 1 = 3
 // 3을 더함!
-console.log('난수*9 올림+3:',(Math.ceil(rdm)+3));
+console.log("난수*9 올림+3:", Math.ceil(rdm) + 3);
 
 /**************************************** 
     [ 내가 원하는 난수 만들기 ]
@@ -187,3 +187,85 @@ console.log('난수*9 올림+3:',(Math.ceil(rdm)+3));
 
 
 ****************************************/
+
+// 이미지 웹경로 배열
+const rimg = [
+  "https://img.etnews.com/photonews/2110/1461216_20211007085904_466_0001.jpg",
+  "https://i.namu.wiki/i/bFpYClpzX-ErL1Kdad69t09fhPPeVb2VwOB_qpH-9qxQ3Tx2Nz9zVIiLPWSMkvnVG5XWnUdQM5hHrGZ4cWneZA.webp",
+  "https://img.imbc.com/adams/Program/202111/132804027350463581.jpg",
+  "https://image.ytn.co.kr/general/jpg/2021/0925/202109251350012465_d.jpg",
+  "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA0MDhfMTE0%2FMDAxNzEyNTQ1NzM5NzY4.CV2JuRLzs91cZq5shllTYdMDo3I1R_4OH1dI_YXcMQUg.f4SqGMxPZxxJK2pM7tWUUACuHuyCc4MagUl-lIgk8ugg.JPEG%2F0.jpg&type=sc960_832",
+  "https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F5772%2F2024%2F05%2F13%2F0000035704_001_20240513113602787.jpg&type=sc960_832",
+  "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA0MTlfMTYy%2FMDAxNzEzNTI2MjE5Mzcy.Mys4WMAjK_r6yuPhS51-zQDbUrqOxhceeEYtB5dtKsYg.Sdm58bbJDhJzZ6fhRnPEN2roaXbatSz16IDXe5NhcM0g.JPEG%2F%25BC%25F6%25BB%25E7.JPG&type=sc960_832",
+  "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDA1MDRfMjYy%2FMDAxNzE0Nzk1MjExNzQw._LK_QpLL0eoy7AVSDfW41pO5Fiva-7wQYvgFjkKj5UIg.TK26jlIQKHLgGImBnWxH8LKnDWoXRmvT2YQGSyV9m2Ig.JPEG%2FUPEqlo.jpg&type=sc960_832",
+];
+
+// 배열개수
+const rimgCnt = rimg.length;
+
+// 1. 요구사항 : 웹경로 이미지를 화면에 넣고 랜덤하게
+// 이미지를 칼라로 약간커지게 클래스 on을 주어서 변경함!
+
+// 2. 대상선정: .imbx
+const imbx = mFn.qs(".imbx");
+
+// 3. 이미지넣기
+// 배열만큼 이미지 넣기
+
+// map으로 이미지넣기
+imbx.innerHTML = rimg
+  .map(
+    (v) => `
+      <div>
+          <img src="${v}" alt="드라마 포스터">
+      </div>
+  `
+  )
+  .join("");
+
+// forEach로 배열을 먼저 사용하여 이미지 넣기
+// rimg.forEach(v=>{
+//     imbx.innerHTML +=
+//     `
+//         <div>
+//             <img src="${v}" alt="드라마 포스터">
+//         </div>
+//     `;
+// })
+
+// 4. 랜덤 처리 대상 div 선택하기
+const target = mFn.qsa(".imbx div");
+console.log("랜덤대상:", target);
+
+// 전에 발생한 난수 저장변수(함수호출전 선언해야함!)
+let beNum = 99999;
+// 초기값은 해당 난수범위외의 수(처음에 통과!)
+
+// 함수최초호출
+randomAddOn();
+// 인터발호출
+setInterval(randomAddOn, 1500);
+
+// 5. 랜덤 처리함수 만들기 ///////
+function randomAddOn() {
+  // 1. 먼저 난수를 발생시킨다!
+  let rdm = Math.floor(Math.random() * rimgCnt);
+  console.log("난수:", rdm);
+
+  // 2. 전에 발생한 난수와 같으면 다시한번 난수발생
+  // 직전 난수는 beNum에 저장함
+  while (rdm == beNum) {
+    rdm = Math.floor(Math.random() * rimgCnt);
+    console.log("다시난수:", rdm);
+  } //////// while ////
+
+  // 3.while문 통과후 결정된 난수를 직전난수 변수에 할당
+  beNum = rdm;
+
+  // 4. 랜덤으로 발생한 난수에 해당하는 div에
+  // on클래스를 추가한다!(나머지는 on제거)
+  target.forEach((ele, i) => {
+    if (i == rdm) ele.classList.add("on");
+    else ele.classList.remove("on");
+  });
+} //////// randomAddOn 함수 ////////
