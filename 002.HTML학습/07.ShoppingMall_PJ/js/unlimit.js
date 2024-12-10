fe385e8720884c7084ba1d2cc6000922a3ef6a6d
Tom B. Parker <tombap8@hotmail.com>
2024-12-10 12:00:05 +0900

    Update unlimit.js

--- 002.HTML학습/07.ShoppingMall_PJ/js/unlimit.js
+++ 002.HTML학습/07.ShoppingMall_PJ/js/unlimit.js
@@ -11,18 +11,20 @@ import myFn from "./my_function.js";
     4. 기능 설계:
 
         (1) 오른쪽 버튼 클릭시 다음 슬라이드가
-            나타나도록 슬라이드 박스의 left값을
+            나타나도록 슬라이드 박스의 
+            translate X축 이동값을
             -100%로 변경시킨다.
             -> 슬라이드 이동후!!! 
             바깥에 나가있는 첫번째 슬라이드
             li를 잘라서 맨뒤로 보낸다!
-            동시에 left값을 0으로 변경한다!
+            동시에 translate X축 이동값을 0으로 변경한다!
 
         (2) 왼쪽버튼 클릭시 이전 슬라이드가
             나타나도록 하기위해 우선 맨뒤 li를
-            맨앞으로 이동하고 동시에 left값을
+            맨앞으로 이동하고 동시에 
+            translate X축 이동값을
             -100%로 변경한다.
-            그 후 left값을 0으로 애니메이션하여
+            그 후 translate X축 이동값을 0으로 애니메이션하여
             슬라이드가 왼쪽에서 들어온다.
 
         (3) 공통기능: 슬라이드 위치표시 블릿
@@ -81,55 +83,40 @@ function goSlide() {
   console.log("나함수!", isRight);
 
   // 3. 분기하여 슬라이드순번 변수 증감하기
-  // (1) 오른쪽일때 증가
+  // (1) 오른쪽버튼 클릭시 : 왼쪽으로 이동
   if (isRight) {
-    seqNum++;
-    // 한계값 설정 :
-    // -> 마지막 슬라이드 순번보다 크면 마지막번호고정
-    // 위에서 슬라이드개수는 상수 SLIDE_CNT에 있음!
-    if (seqNum > SLIDE_CNT - 1) seqNum = SLIDE_CNT - 1;
-  } /// if ///
-  // (2) 왼쪽일때 감소
+    // [1] translate x축방향 -100% 이동
+    slide.style.translate = '-100%';
+    slide.style.transition = '.4s ease-in-out';
+
+    // [ 슬라이드 이동후 실행해야함 ]
+    // 따라서 setTimeout으로 시간 지연실행코드작성
+    setTimeout(() => {
+        // [2] 맨앞li 선택하여 맨뒤로 이동하기
+        // 슬라이드.appendChild(맨앞li)
+        slide.appendChild(myFn.qsaEl(slide,'li')[0]);
+        
+    }, 400); /// setTimeout ///
+  } /////////// if ///////////
+  // (2) 왼쪽버튼 클릭시 : 오른쪽으로 이동
   else {
-    seqNum--;
-    // 한계값설정 :
-    // -> 0보다 작아지면 0으로 고정
-    if (seqNum < 0) seqNum = 0;
-  } /// else ///
+    
+  } ///////// else ///////
 
-  console.log("슬순번:", seqNum);
-
-  // 4. 슬라이드 CSS변경하여 슬라이드 이동하기
-  slide.style.translate = seqNum * -100 + "%";
-  slide.style.transition = ".4s ease-in-out";
-
-  // 5. 순번이 끝번호일때 버튼 숨기기 보이기 처리
-  if (seqNum === 0) {
-    // 처음슬라이드 왼쪽버튼 숨기기
-    abtn[0].style.display = "none";
-  } /// if ///
-  else if (seqNum === SLIDE_CNT - 1) {
-    // 마지막슬라이드 오른쪽버튼 숨기기
-    abtn[1].style.display = "none";
-  } // else if ///
-  else {
-    // 기타경우 모두보이기
-    abtn[0].style.display = "block";
-    abtn[1].style.display = "block";
-  } /// else ///
+  
 
   // 6. 인디케이터 변경하기 : 대상 .indic li
-  indic.forEach((el, idx) => {
-    // console.log(el,idx);
-    // (1) 페이지번호와 일치하는 순번li에 클래스 "on"넣기
-    if (idx === seqNum) {
-      el.classList.add("on");
-    } /// if ///
-    // (2) 나머지 li는 "on" 제거하기
-    else {
-      el.classList.remove("on");
-    } /// else ///
-  }); /// forEach ////
+//   indic.forEach((el, idx) => {
+//     // console.log(el,idx);
+//     // (1) 페이지번호와 일치하는 순번li에 클래스 "on"넣기
+//     if (idx === seqNum) {
+//       el.classList.add("on");
+//     } /// if ///
+//     // (2) 나머지 li는 "on" 제거하기
+//     else {
+//       el.classList.remove("on");
+//     } /// else ///
+//   }); /// forEach ////
 
   // -> seqNum 값 즉, 슬라이드 순번과
   // 인디케이터 li 순번이 같으므로
