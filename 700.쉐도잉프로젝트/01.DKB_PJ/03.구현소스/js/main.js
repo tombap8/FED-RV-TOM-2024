b70b604f2f570f475796b1b6c246159244ab2d23
Tom B. Parker <tombap8@hotmail.com>
2024-12-17 14:40:57 +0900

    ss

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -49,5 +49,31 @@ const swiper = new Swiper(".clip-box", {
   slidesPerView: 4,
   // 슬라이드 사이 간격(숫자는 px단위임)
   spaceBetween: 0,
-  
 }); /// swiper /////
+
+// 스와이퍼 API를 이용한 개별 코딩
+// 내가 만든 버튼에 이동 기능 부여하기!
+const myBtns = document.querySelectorAll(".btn-box .abtn");
+console.log("대상:", myBtns);
+
+// 클릭이벤트 설정하기
+myBtns.forEach((el) => {
+  el.addEventListener("click", controlSwp);
+});
+
+// 기능구현함수
+function controlSwp() {
+  // 버튼구분하기
+  let isBtn = this.classList.contains("ab2");
+  console.log("나야나!", isBtn);
+
+  // 분기하기
+  // 다음버튼
+  if (isBtn) {
+    swiper.slideNext();
+  } // if //
+  // 이전버튼
+  else {
+    swiper.slidePrev();
+  } // else //
+} ////////// controlSwp함수 /////////
