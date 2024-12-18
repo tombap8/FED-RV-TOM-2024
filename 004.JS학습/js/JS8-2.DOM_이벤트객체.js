584a9003bb4e565ceeff08e664a7e035980adf14
Tom B. Parker <tombap8@hotmail.com>
2024-12-18 12:57:50 +0900

    Update JS8-2.DOM_이벤트객체.js

--- 004.JS학습/js/JS8-2.DOM_이벤트객체.js
+++ 004.JS학습/js/JS8-2.DOM_이벤트객체.js
@@ -6,75 +6,75 @@ import myFn from "./my_function.js";
 // console.log(myFn);
 
 /// 이벤트 객체의 이벤트 로그찍기함수 ///
-const showEvtLog = x => 
-    console.log(x,":",
-    "\n currentTarget:",event.currentTarget,
-    "\n target:",event.target,
-    "\n cancelable:",event.cancelable,
-    "\n bubbles:",event.bubbles,
-    "\n type:",event.type,
-    "\n view:",event.view,
-    "\n eventPhase:",event.eventPhase,
-    "\n isTrusted:",event.isTrusted,
-
-);
-
+const showEvtLog = (x) =>
+  console.log(
+    x,
+    ":",
+    "\n currentTarget:",
+    event.currentTarget,
+    "\n target:",
+    event.target,
+    "\n cancelable:",
+    event.cancelable,
+    "\n bubbles:",
+    event.bubbles,
+    "\n type:",
+    event.type,
+    "\n view:",
+    event.view,
+    "\n eventPhase:",
+    event.eventPhase,
+    "\n isTrusted:",
+    event.isTrusted
+  );
 
 // 이벤트 전달을 관찰하기 위한 클릭이벤트 설정 ///
 
 // 1. 아들방 ///////
-myFn.qs('a').onclick = (e) => {
-    // e - 이벤트 전달변수
-    // 이벤트 버블링 막기
-    // event.stopPropagation();
-    
-    e.currentTarget // 이벤트발생요소
-    .style.backgroundColor = 'red';
-    
-    // 이벤트 로그 찍기
-    showEvtLog('아들방(a)');
+myFn.qs("a").onclick = (e) => {
+  // e - 이벤트 전달변수
+  // 이벤트 버블링 막기
+  // event.stopPropagation();
 
-    // 기본기능막기(a요소는 이동특성이 있음. 이것을 막음!)
-    // event.preventDefault();
-    return false;
-    // 리턴시 false를 쓰면 기본기능막기가 됨!
+  e.currentTarget.style.backgroundColor = "red"; // 이벤트발생요소
+
+  // 이벤트 로그 찍기
+  showEvtLog("아들방(a)");
+
+  // 기본기능막기(a요소는 이동특성이 있음. 이것을 막음!)
+  // event.preventDefault();
+  return false;
+  // 리턴시 false를 쓰면 기본기능막기가 됨!
 }; /// click ///
 
 // 2. 엄마집 ///////
-myFn.qs('p').onclick = function(e)  {
-    // e - 이벤트 전달변수
-    // e.stopPropagation();
-    e.currentTarget // 이벤트발생요소
-    .style.backgroundColor = 'yellow';
-    // return false;
+myFn.qs("p").onclick = function (e) {
+  // e - 이벤트 전달변수
+  // e.stopPropagation();
+  e.currentTarget.style.backgroundColor = "yellow"; // 이벤트발생요소
+  // return false;
 
-    // 이벤트 로그 찍기
-    showEvtLog('엄마집(p)');
+  // 이벤트 로그 찍기
+  showEvtLog("엄마집(p)");
 }; /// click ///
 
-
-
 // 3. 친척네집 //////
-myFn.qs('div').onclick = (e) => {
-    // e - 이벤트 전달변수
-    e.currentTarget // 이벤트발생요소
-    .style.backgroundColor = 'green';
-    
-    // 이벤트 로그 찍기
-    showEvtLog('친척네집(div)');
+myFn.qs("div").onclick = (e) => {
+  // e - 이벤트 전달변수
+  e.currentTarget.style.backgroundColor = "green"; // 이벤트발생요소
+
+  // 이벤트 로그 찍기
+  showEvtLog("친척네집(div)");
 }; /// click ///
 
 // 4. 대한민국 전체 /////
-myFn.qs('body').onclick = (e) => {
-    // e - 이벤트 전달변수
-    e.currentTarget // 이벤트발생요소
-    .style.backgroundColor = 'blue';
-    
-    // 이벤트 로그 찍기
-    showEvtLog('대한민국전체(body)');
+myFn.qs("body").onclick = (e) => {
+  // e - 이벤트 전달변수
+  e.currentTarget.style.backgroundColor = "blue"; // 이벤트발생요소
+
+  // 이벤트 로그 찍기
+  showEvtLog("대한민국전체(body)");
 }; /// click ///
-
-
 
 /**************************************************
     
@@ -176,3 +176,21 @@ stopImmediatePropagation() 메서드 사용!
 -> 주의: 함수를 외부함수로 만들어야 해제가 가능하다!
 (2) 이벤트속성 = ""; -> 빈 문자열값을 할당하여 지움
 **************************************************/
+
+// 휠 이벤트 막기 테스트 ////
+window.addEventListener(
+  "wheel",
+  () => {
+    // 기본기능막기
+    event.preventDefault();
+
+    console.log("휠~~~~");
+    // 배경색 변경
+    myFn.qs("body").style.backgroundColor = "lightcoral";
+  },
+  { passive: false }
+);
+// 이벤트막기할 경우 window, body, document
+// 일 경우 막기를 할 수 없게 셋팅됨!
+// 이것을 쓰려면 이벤트 설정시 맨뒤에 {passive:false}
+// 설정을 추가한다!
