6f9b91b1e59ca95ea4a815ebecab9b69268a16b6
Tom B. Parker <tombap8@hotmail.com>
2024-12-23 12:04:52 +0900

    Update common.js

--- 999.코드실험실/03.링크시스템/js/common.js
+++ 999.코드실험실/03.링크시스템/js/common.js
@@ -5,10 +5,10 @@
 // html과 CSS 원본에 셋팅하는 것이 해결책이 된다!
 // $("body").append(`<div class="pg-cover"></div>`);
 
-// 1. 가림막요소 변수할당
+// 1. 가림막요소 변수할당 //////
 const $pgCover = $(".pg-cover");
 
-// 2. 최초화면 셋팅 및 애니설정
+// 2. 최초화면 셋팅 및 애니설정 ///////
 $pgCover
   // .css({ height: "100vh" }) -> CSS에 미리해줌!
   .animate({ height: "0", top: "100%" }, 800, "easeInOutExpo", function () {
@@ -20,7 +20,7 @@ $pgCover
 // 여기에서 0.8초간 화면가리막 요소 내려오게 애니함
 // 즉, .gnb a 클릭시!!
 
-// 3. 이동클릭시 가림막 내려오기 애니
+// 3. 이동클릭시 가림막 내려오기 애니 ///////
 $(".gnb a").click(() => {
   $pgCover.animate(
     {
