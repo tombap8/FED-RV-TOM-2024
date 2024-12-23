d542d8de508a23e0d86b26dc330bffb2c3423f84
Tom B. Parker <tombap8@hotmail.com>
2024-12-23 10:15:33 +0900

    ss

---
+++ 999.코드실험실/02.화면전환효과/js/main.js
@@ -0,0 +1,13 @@
+// 화면전환효과 JS
+
+// 버튼 클릭시 서브 페이지 변경할 경우
+// 전환을 위한 클래스 넣기
+// 단, SPA(Single Page Application) 구성시 예시임
+// MPA(Multi Page Application)일 경우 달라짐!
+
+const isrc = {
+    HOME: "https://t4.ftcdn.net/jpg/02/96/64/61/360_F_296646140_btnypVWa1CHefYgC3EgoRLzO2Qh79ITa.jpg",
+    WORK: "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2021/02/25083908/How-to-Find-Online-Jobs-Work-From-Home-Jobs-That-Require-Little-or-No-Work-Experience.jpg",
+    CONTACT:
+      "https://images.unsplash.com/photo-1423666639041-f56000c27a9a?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29udGFjdHxlbnwwfHwwfHx8MA%3D%3D",
+  };
