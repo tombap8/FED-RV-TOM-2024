457e6fa90ef76e6996fb9e90b79cef0e4b7db931
Tom B. Parker <tombap8@hotmail.com>
2024-12-23 11:17:15 +0900

    ss

---
+++ 999.코드실험실/03.링크시스템/js/linksys.js
@@ -0,0 +1,25 @@
+// 링크 시스템 JS
+
+// GNB메뉴 a링크 처리 하기
+// 대상: .gnb a
+$('.gnb a').click(function(e){
+    e.preventDefault();
+
+    let txt = $(this).text();
+
+    // 분기문
+    let url;
+    // 버튼에 따른 주소 변수할당
+    switch(txt){
+        case 'HOME': url = 'index'; break;
+        case 'WORK': url = 'sub1'; break;
+        case 'CONTACT': url = 'sub2'; break;
+    }
+
+    // 페이지 이동하기
+    // 같은 시스템은 같은 창에서 이동함
+    // location.href = 이동할주소
+    location.href = `./${url}.html`;
+
+
+});
