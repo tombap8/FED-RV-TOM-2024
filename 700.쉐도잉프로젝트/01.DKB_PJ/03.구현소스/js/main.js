a5daca5dbd523ba1d2340d584c25a6e64b6ea27a
Tom B. Parker <tombap8@hotmail.com>
2024-12-27 16:01:19 +0900

    ss

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -82,18 +82,46 @@ $('.poster-box').html(
   '<ul>' +
   dkbData.posterData.map(v=>`
     <li data-idx="${v.idx}">
-      <figure>
-        <img
-          src="./images/poster_img/${v.imgName}.jpg"
-          alt="${v.title}"
-        />
-        <figcaption>${v.title}</figcaption>
-      </figure>
+    <figure>
+    <img
+    src="./images/poster_img/${v.imgName}.jpg"
+    alt="${v.title}"
+    />
+    <figcaption>${v.title}</figcaption>
+    </figure>
     </li>
-  `).join('')
-  + '</ul>'
-);
+    `).join('')
+    + '</ul>'
+  );
+  
+  
+  
+  /////////////////////////////////////////////////
+  /// 최신동영상영역 : 데이터 연결하여 태그 만들기 ///
+  /////////////////////////////////////////////////
 
+  // 대상: .clip-box
+  $('.clip-box').html(
+    `<ul class="slide swiper-wrapper" data-db="clipData">
+    ${
+      dkbData.clipData.map(v=>`
+        <li class="swiper-slide" data-idx="${v.idx}">
+          <div class="clip-mv-box">
+            <img
+              src="./images/clip_img/${v.idx}.jpg"
+              alt="${v.subtit}"
+            />
+          </div>
+          <h4>
+            ${v.subtit}
+          </h4>
+          <h3>${v.title}</h3>
+        </li>        
+      `).join('')
+    }
+      </ul>
+    `
+  );
 
 
 
