e023d0dc7da89d36919d03ad8dae965ef59002af
Tom B. Parker <tombap8@hotmail.com>
2025-01-09 15:39:07 +0900

    ss

--- 004.JS학습/01.JS실험실/js/scroll_menu.js
+++ 004.JS학습/01.JS실험실/js/scroll_menu.js
@@ -1,2 +1,8 @@
 // 스크롤 방향에 따른 메뉴 보이기/숨기기 JS
 // - scroll_menu.js
+
+import myFn from './my_function.js';
+
+myFn.addEvt(window,'wheel',(e)=>{
+    console.log('휠',e.wheelDelta);
+}); ///////// wheel 이벤트 함수 ////////
