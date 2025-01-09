0f7736c8a2614e45c20169ddf01ed8aaec909908
Tom B. Parker <tombap8@hotmail.com>
2025-01-09 15:42:31 +0900

    Update scroll_menu.js

--- 004.JS학습/01.JS실험실/js/scroll_menu.js
+++ 004.JS학습/01.JS실험실/js/scroll_menu.js
@@ -3,6 +3,17 @@
 
 import myFn from './my_function.js';
 
+
+// 대상: #header
+const headerMenu = myFn.qs('#header');
+
 myFn.addEvt(window,'wheel',(e)=>{
-    console.log('휠',e.wheelDelta);
+    let delta = e.wheelDelta;
+    console.log('휠',delta);
+
+    // wheelDelta값이 마이너스는 아랫방향
+    // -> #header에 .on을 줘서 숨기기
+    if(delta < 0) headerMenu.classList.add('on');
+    else headerMenu.classList.remove('on');
+
 }); ///////// wheel 이벤트 함수 ////////
