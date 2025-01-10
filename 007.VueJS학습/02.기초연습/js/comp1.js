9efef6e40a6f9cc4f81e9eddcaa780f54bd5a981
Tom B. Parker <tombap8@hotmail.com>
2025-01-10 15:49:47 +0900

    ss

--- 007.VueJS학습/02.기초연습/js/comp1.js
+++ 007.VueJS학습/02.기초연습/js/comp1.js
@@ -1,1 +1,16 @@
 // 01. 컴포넌트 연습1 JS
+
+// 1. 상단영역 전역 컴포넌트 만들기
+// Vue.component(컴포넌트명,{template:코드})
+Vue.component("tit-comp", {
+  template: `
+    <strong>
+        <span>
+          😉Vue JS😎 컴포넌트 :
+        </span>
+        쇼핑모~~~올 갤러리 리스트
+      </strong>
+    `,
+}); ///// 전역컴포넌트1 /////
+
+new Vue({el:'.tit'});
