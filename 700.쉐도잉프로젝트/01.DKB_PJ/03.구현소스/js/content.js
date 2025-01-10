844766b3ceb3f5907b8eddbd5c3f752178a0c13f
Tom B. Parker <tombap8@hotmail.com>
2025-01-10 12:17:58 +0900

    Update content.js

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/content.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/content.js
@@ -4,7 +4,11 @@
 import comFn from "./common.js";
 comFn();//실행!
 
-// 파라미터 확인하기!
+// 서브 소스 데이터 가져오기
+import subSource from "../data/sub_data.js";
+// console.log(subSource);
+
+// 1. 파라미터 확인하기!
 // URL로 넘어온 물음표가 없으면 첫페이지로 돌려보내기
 
 let pm = location.search;
@@ -15,8 +19,14 @@ if(pm.indexOf("?") == -1) {
     location.href = "index.html";
 } // if //
 
-// 물음표가 있는 파라미터 잘라내기!
+// 2. 물음표가 있는 파라미터 잘라내기!
 pm = pm.split('=')[1];
-console.log('원하는값:',pm);
+console.log('원하는값:',pm,subSource[pm]);
+
+// 3. 데이터 매칭하여 html 넣기
+// 대상: .main-area
+// 데이터: subSource["login/join/cat"]
+$('.main-area').html(subSource[pm]);
+
 
 
