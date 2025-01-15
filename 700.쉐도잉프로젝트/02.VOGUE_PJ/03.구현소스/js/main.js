b5b73f1782f69ad76b56f0572d101b0ce6db8fc7
Tom B. Parker <tombap8@hotmail.com>
2025-01-15 12:10:58 +0900

    ss

---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/main.js
@@ -0,0 +1,27 @@
+// 보그 PJ 메인페이지 JS - main.js
+
+// 아이템 데이터 불러오기
+import itemData from '../data/item_data.json' with{type:'json'};
+
+console.log(itemData);
+
+// 패션 영역 셋팅 ///
+document.querySelector('.post-list')
+.innerHTML = `
+    <ul>
+        ${
+            itemData.map(v=>`                
+                <li>
+                <figure>
+                    <img src="${v.imgSrc}" alt="${v.mainCat}">
+                    <figcaption>
+                    <p class="category">${v.catName}</p>
+                    <h3 class="s-tit">${v.title}</h3>
+                    <p class="date">${v.dateWriter}</p>
+                    </figcaption>
+                </figure>
+                </li>
+            `).join('')
+        }
+    </ul>
+`;
