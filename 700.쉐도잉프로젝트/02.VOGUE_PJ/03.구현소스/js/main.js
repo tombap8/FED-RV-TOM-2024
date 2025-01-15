8d8ad1c32a2eca86710b072301955ca9966b1ce4
Tom B. Parker <tombap8@hotmail.com>
2025-01-15 15:54:45 +0900

    ss

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/main.js
@@ -41,3 +41,40 @@ document.querySelector(".post-list").innerHTML = `
           .join("")}
     </ul>
 `;
+
+// 3. 투데이 영역 구현하기 ///////
+
+// 3-1. 투데이 영역용 데이터수집하기
+// filter() 메서드는 조건이 맞을때 true리턴!
+// -> 결과: 필터링된 배열값
+const todayData = itemData.filter(v=>{
+    if(v.mainCat=='today')return true
+})
+// 3-2. 투데이 영역 데이터 정렬하기 : idx 오름차순
+// sort((a,b)=>a.idx==b.idx?0:a.idx<b.idx?-1:1)
+.sort((a,b)=>a.idx==b.idx?0:a.idx<b.idx?-1:1)
+
+
+console.log('today데이터:',todayData);
+
+// 3-3. 패션 영역 셋팅 ///
+document.querySelector(".post-list-today").innerHTML = `
+    <ul>
+        ${todayData
+          .map(
+            (v) => `                
+                <li>
+                <figure>
+                    <img src="${v.imgSrc}" alt="${v.mainCat}">
+                    <figcaption>
+                    <p class="category">${v.catName}</p>
+                    <h3 class="s-tit">${v.title}</h3>
+                    <p class="date">${v.dateWriter}</p>
+                    </figcaption>
+                </figure>
+                </li>
+            `
+          )
+          .join("")}
+    </ul>
+`;
