9a06a9744edf040c016a0bcb5e52c312e55f8479
Tom B. Parker <tombap8@hotmail.com>
2025-01-16 14:42:44 +0900

    Update vuex_store.js

--- 007.VueJS학습/02.기초연습/js/vuex_store.js
+++ 007.VueJS학습/02.기초연습/js/vuex_store.js
@@ -26,7 +26,7 @@ Vue.component('main-area',{
     template: `
         <main>
             <img src="https://img.freepik.com/premium-vector/city-illustration_23-2147514701.jpg" alt="지역이미지" />
-            
+
             <p>
                 도시소개에 오신것을 환영합니다!
             </p>
@@ -42,7 +42,13 @@ Vue.component('main-area',{
 // (3) 하단영역 컴포넌트
 Vue.component('info-area',{
     // 템플릿설정
-    template: ``,
+    template: `
+        <footer>
+            <address>
+                서울시 강남구 역삼동 119 뷰엑스B
+            </address>
+        </footer>
+    `,
     // 데이타설정 : 컴포넌트는 객체리턴메서드로 해야함!
     data(){
         return{};
