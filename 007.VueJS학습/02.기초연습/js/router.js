823dbba08a0342bbcc8c703623b72db5c9d1e444
Tom B. Parker <tombap8@hotmail.com>
2025-01-17 14:32:24 +0900

    SS

---
+++ 007.VueJS학습/02.기초연습/js/router.js
@@ -0,0 +1,10 @@
+// 뷰 라우터 셋팅 JS : router.js
+
+// 뷰라우터 인스턴스 내보내기 /////
+// -> 라우터 셋팅 방식
+// new VueRouter({routes:[]})
+// -> 만약 외부에 셋팅될 경우
+// -> const routes = []
+// -> new VueRouter({routes})
+// -> routes라는 속성명은 정해진이름임!
+export default new VueRouter({});
---
+++ 007.VueJS학습/02.기초연습/js/vue_router.js
@@ -0,0 +1,4 @@
+// 05.Vue Router JS : vue_router.js
+
+// 라우터 셋팅 JS 불러오기
+import router from './router.js';
