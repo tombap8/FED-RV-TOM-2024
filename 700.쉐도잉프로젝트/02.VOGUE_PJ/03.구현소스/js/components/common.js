6cc5d70074575b2c8e4520a022aa6417f3ee76b0
Tom B. Parker <tombap8@hotmail.com>
2025-01-21 17:40:05 +0900

    ss

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/components/common.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/components/common.js
@@ -44,7 +44,42 @@ const TopComp = Vue.component('top-comp',{
     },
 });
 // 2. 하단컴포넌트
-const BottomComp = Vue.component('bottom-comp',{});
+const BottomComp = Vue.component('bottom-comp',{
+  // 템플릿 코드 ////
+  template: `
+    <div id="footer-area">
+        <footer class="footer-area ibx common-area">
+          <!-- 3-1.하단로고 -->
+          <div class="blogo">
+            <img src="./images/svg/logo_white_m.svg" alt="하단로고" />
+          </div>
+          <!-- 3-2.하단링크 -->
+          <ul class="blink">
+            <li>
+              <a href="#">정기구독</a>
+            </li>
+            <li>
+              <a href="#">회사소개</a>
+            </li>
+            <li>
+              <a href="#">광고 및 제휴</a>
+            </li>
+            <li>
+              <a href="#">개인정보 처리방침</a>
+            </li>
+          </ul>
+          <!-- 3-3.회사주소 -->
+          <address class="addr">
+            VOGUE.CO.KR IS OPERATED BY DOOSAN MAGAZINE
+          </address>
+        </footer>
+      </div>
+  `,
+  // 데이터 설정 ////
+  data(){
+    return{};
+  },
+});
 
 // 3. 내보내기
 export {TopComp,BottomComp};
