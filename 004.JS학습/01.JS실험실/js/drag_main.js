db83500e2effbc872c12c5694bf99584ff35b9cd
Tom B. Parker <tombap8@hotmail.com>
2025-01-22 13:07:34 +0900

    ss

--- 004.JS학습/01.JS실험실/js/drag_main.js
+++ 004.JS학습/01.JS실험실/js/drag_main.js
@@ -1,7 +1,7 @@
 // 드래그 기능 메인 페이지 JS - drag_main.js
 
-import SetDrag from "./drag.js";
+import setDrag from "./drag.js";
 
 // 드래그 기능 함수 호출하기
 // 드래그 대상 클래스 이름만 보내준다!
-new SetDrag('dtg');
+setDrag('dtg');
