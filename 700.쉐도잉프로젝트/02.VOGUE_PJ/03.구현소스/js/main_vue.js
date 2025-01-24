411a04c48c417f9ef1b123e16b85e8f1a10c6844
Tom B. Parker <tombap8@hotmail.com>
2025-01-24 14:40:56 +0900

    ss

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/main_vue.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/main_vue.js
@@ -3,8 +3,9 @@
 // [1] 공통컴포넌트
 import { TopComp } from "./components/common.js";
 import { BannerComp } from "./components/banner.js";
+import { JoinComp } from "./components/join.js";
 
-console.log(TopComp, BannerComp);
+console.log(TopComp, BannerComp, JoinComp);
 
 /******************************* 
     메인 뷰 인스턴스 생성하기 
