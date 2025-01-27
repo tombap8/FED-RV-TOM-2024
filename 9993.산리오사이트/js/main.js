f3033cb1225f1f3880a63aab6adcf1ddbdd59408
Tom B. Parker <tombap8@hotmail.com>
2025-01-27 12:19:09 +0900

    ss

--- 9993.산리오사이트/js/main.js
+++ 9993.산리오사이트/js/main.js
@@ -2,3 +2,10 @@
 
 import myFn from './my_function.js';
 
+
+// 1. GNB메뉴 대문자글자변경 ////
+myFn.qsa(".gnb ul li")
+.forEach(el=>el.innerText=
+    el.innerText.toUpperCase());
+// toUpperCase() 대문자변경
+// toLowerCase() 소문자변경
