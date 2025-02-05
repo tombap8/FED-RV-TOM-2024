8980b64a039d0eedba2103e13b2b311dbb491d7d
Tom B. Parker <tombap8@hotmail.com>
2025-02-05 12:21:10 +0900

    aa

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/main_vue_spa.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/main_vue_spa.js
@@ -1,14 +1,4 @@
 // 보그 PJ 메인페이지 뷰적용 JS - main_vue.js
-// 컴포넌트 불러오기 /////
-// [1] 공통컴포넌트
-import { TopComp } from "./components/common.js";
-import { BannerComp } from "./components/banner.js";
-import { JoinComp } from "./components/join.js";
-import { LoginComp } from "./components/login.js";
-import { TodayAreaComp } from "./components/today_area.js";
-import { FashionAreaComp } from "./components/fashion_area.js";
-
-// console.log(TopComp, BannerComp, JoinComp, LoginComp);
 
 /******************************* 
     메인 뷰 인스턴스 생성하기 
---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/router.js
@@ -0,0 +1,48 @@
+// 보그 PJ - 뷰 라우터 셋팅 JS - router.js
+
+// 컴포넌트 불러오기 /////
+// [1] 공통컴포넌트
+import { TopComp } from "./components/common_spa.js";
+import { BannerComp } from "./components/banner.js";
+import { JoinComp } from "./components/join.js";
+import { LoginComp } from "./components/login.js";
+import { TodayAreaComp } from "./components/today_area.js";
+import { FashionAreaComp } from "./components/fashion_area.js";
+
+// console.log(TopComp, BannerComp, JoinComp, LoginComp);
+
+// 메인페이지 컴포넌트 : 구성할 여러개의 컴포넌트호출 ////
+const MainComp = Vue.component("main-comp",{
+    template: `
+        <main>
+            <!-- 배너영역 -->
+            <banner-comp></banner-comp>
+
+            <!-- Today’s Stories 영역 -->
+            <today-area-comp></today-area-comp>
+
+            <!-- fashion 영역 -->
+            <fashion-area-comp></fashion-area-comp>
+        </main>
+    `
+}); ///////////// MainComp ///////////////////
+
+// [ 뷰라우터 인스턴스 내보내기 ] /////
+// -> 라우터 셋팅 방식
+// new VueRouter({routes:[]})
+// -> 만약 외부에 셋팅될 경우
+// -> const routes = []
+// -> new VueRouter({routes})
+// -> routes라는 속성명은 정해진이름임!
+export default new VueRouter({
+    routes: [
+      {
+        // (1) 경로설정 : path
+        // -> router-link의 to 속성값과 같은값으로 셋팅!
+        path: "/", // 슬래쉬(/)만 쓰면 첫페이지임!
+        // (2) 연결할 컴포넌트 설정 : component
+        // -> 외부의 변수로 셋팅할 수 있고 직접 쓸 수 있음
+        component: MainComp,
+      },
+  ],
+  }); ///////// VueRouter ////////////////
