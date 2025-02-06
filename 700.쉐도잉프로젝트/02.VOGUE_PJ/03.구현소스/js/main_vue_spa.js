962f03162caaf7c4febf48209b837ce86efd8b9a
Tom B. Parker <tombap8@hotmail.com>
2025-02-06 15:02:51 +0900

    ss

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/main_vue_spa.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/main_vue_spa.js
@@ -18,7 +18,22 @@ new Vue({
     // 2. 데이터설정
     data:{},
     // 3. 메서드설정
-    methods:{},
+    methods:{
+        // 최초 로그인 확인하기
+        // 로그인 상태라면 세션스토리지값을 읽어옴
+        // 왜? 로컬스가 아닌 세션스인가?
+        // 로그인 상태는 브라우저가 떠 있을동안만 필요!
+        initSet(){
+            let temp;
+            // 세션스 'login-user'가 있으면 처리!
+            if(sessionStorage.getItem('login-user')){
+                // 세션스값 객체변환
+               temp = JSON.parse(sessionStorage.getItem('login-user'));
+               // 뮤테이션 로그인 셋 메서드 호출
+               store.commit('setLogin',temp);
+            }
+        },
+    },
     // 4. 라이프사이클 메서드
     // 4-1. created() : 데이터생성관련코드 작성
     created(){},
