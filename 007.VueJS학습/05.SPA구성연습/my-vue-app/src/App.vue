c0e33ffadfcb2d0a52091912c961d056e0d8d91d
Tom B. Parker <tombap8@hotmail.com>
2025-02-13 11:33:42 +0900

    ss

--- 007.VueJS학습/05.SPA구성연습/my-vue-app/src/App.vue
+++ 007.VueJS학습/05.SPA구성연습/my-vue-app/src/App.vue
@@ -11,7 +11,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="sass">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,8 +19,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-#app img{
-  border: 10px dotted red;
+
+  img{
+    border: 10px dotted red;
+  }
 }
 </style>
