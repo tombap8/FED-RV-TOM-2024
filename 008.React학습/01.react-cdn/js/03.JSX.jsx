935c43c76b9f5cdc0adbacd06edbb03325768f62
Tom B. Parker <tombap8@hotmail.com>
2025-02-17 12:20:23 +0900

    Update 03.JSX.jsx

--- 008.React학습/01.react-cdn/js/03.JSX.jsx
+++ 008.React학습/01.react-cdn/js/03.JSX.jsx
@@ -6,9 +6,27 @@
     - 리액트에서 HTML을 쉽게 작성할 수 있다.
     - appendChild() 메서드 없이 DOM에 요소넣기가 가능함!
 *****************************************************/
+// 출력할 요소 선택하기 ////
+const target = document.querySelectorAll('#root>div');
 
+// 1. JSX를 사용한 것과 JSX를 사용하지 않을 것을 비교
+// (1) JSX 를 사용한 예
+// (1-1) 넣을 요소 만들기
+const myEle1 = <h1>나는 JSX를 사용하고 있어!</h1>;
+// (1-2) 리액트 루트 선택하기 : createRoot() 메서드 사용!
+const root1 = ReactDOM.createRoot(target[0]);
+// (1-3) 요소 출력하기 //////
+// 생성된 루트에 render() 메서드를 붙여서 사용!
+root1.render(myEle1);
 
+// (2) JSX를 사용하지 않는 방법 //////////////
+// -> 넣을 요소를 createElement() 메서드로 생성해야함!
+const myEle2 = React
+.createElement("h1",{},"나는 JSX를 쓰지 않아!");
+// createElement(요소명,{JS코드작성},요소내용)
 
+// 두번째 div에 출력하기 /////
+ReactDOM.render(myEle2, target[1]);
 
 /***************************************************** 
     [ 출력방식 정리! ]
