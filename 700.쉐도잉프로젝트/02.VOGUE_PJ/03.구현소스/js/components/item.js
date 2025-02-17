671deb2eb7e9141822f3f8d8040b81c45bd7178d
Tom B. Parker <tombap8@hotmail.com>
2025-02-17 15:56:40 +0900

    Update item.js

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/components/item.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현소스/js/components/item.js
@@ -21,15 +21,29 @@ Vue.component("item-comp",{
             </nav>
         </header>
 
-        <!-- Today’s Stories 유형 영역 -->
+        <!-- 
+            [ Today’s Stories 유형 영역 ]
+            list-tit 속성은 타이틀이 없으므로 빈값셋팅
+            data-name 속성은 라우터 query의 id값에 따라
+            데이터 이름을 변경해야하므로 속성을 바인딩하여
+            v-bind:data-name="코드" 로 작성한다! 줄이면
+            :data-name="코드" 로 작성할 수 있다!
+
+            코드로 작성된 $route.query.id는 파라미터로
+            넘어온 값 즉, 카테고리명과 같다!
+            이 값이 대문자 이므로 소문자로 보내려고
+            toLowerCase() 메서드를 사용했다!
+        -->
         <today-area-comp
             list-tit=""
             :data-name="$route.query.id.toLowerCase()"
         ></today-area-comp>
 
+        <!--
         <div style="font-size:10vw">
             출력확인:{{$route.query.id.toLowerCase()}}
         </div>
+        -->
     </main>
     `,
     // 2. 리턴함수 데이터
