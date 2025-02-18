9376deb03f5bfb155611d7011e6c99296369794d
Tom B. Parker <tombap8@hotmail.com>
2025-02-18 11:20:52 +0900

    Update 04.컴포넌트.jsx

--- 008.React학습/01.react-cdn/js/04.컴포넌트.jsx
+++ 008.React학습/01.react-cdn/js/04.컴포넌트.jsx
@@ -3,7 +3,6 @@
 // 내함수 불러오기
 import myFn from "./my_function";
 
-
 /************************************************* 
     [ 리액트 컴포넌트 ]
     - 컴포넌트는 HTML요소를 반환하는 함수다!
@@ -40,23 +39,53 @@ import myFn from "./my_function";
 *************************************************/
 /// [ 클래스형 컴포넌트 만들기 ] /////
 class GoghWork extends React.Component {
-    // 클래스형 컴포넌트에서는 render() 메서드로
-    // HTML 코드를 리턴한다! 내부에 return 구문필요!
-    render(){
-        // html코드 리턴
-        return(
-            <React.Fragment>
-                <h2>안녕! 나는 고흐그림이야!</h2>
-                <img src="./images/01.png" alt="고흐그림" />
-            </React.Fragment>
-        );
-
-    } //// render 메서드 ///
+  // 클래스형 컴포넌트에서는 render() 메서드로
+  // HTML 코드를 리턴한다! 내부에 return 구문필요!
+  render() {
+    // html코드 리턴
+    return (
+      <React.Fragment>
+        <h2>안녕! 나는 고흐그림이야!</h2>
+        <MakeImage isrc="01.png" ialt="고흐그림" />
+        {/* <img src="./images/01.png" alt="고흐그림" /> */}
+      </React.Fragment>
+    );
+  } //// render 메서드 ///
 } ////// GoghWork 클래스형 컴포넌트 ////
 
 // 전체 출력요소 대상 선정하기 ////
-const target = myFn.qsa('.root');
+const target = myFn.qsa(".root");
 
 // 첫번째 .root에 고흐출력하기 ///
 // ReactDOM.render(컴포넌트,출력요소)
 ReactDOM.render(<GoghWork />, target[0]);
+
+// [ 함수형 컴포넌트 만들기 ] //////
+// 첫글자는 대문자!!
+function IronMan() {
+  return (
+    <React.Fragment>
+      <h2>안녕! 나는 아이언맨이야!</h2>
+      <MakeImage isrc="ab1.jpg" ialt="아이언맨" />
+      {/* <img src="./images/ab1.jpg" alt="아이언맨" /> */}
+    </React.Fragment>
+  );
+} /////// IronMan 함수형 컴포넌트 ///////
+
+// 두번째 .root에 출력하기 ////
+ReactDOM.render(<IronMan />, target[1]);
+
+// [ 이미지생성 공통 컴포넌트 ] ////
+// function MakeImage({ isrc, ialt }) {
+function MakeImage(props) {
+  // {ialt, isrc} - 구조분해할당
+  // 객체가 들어올때 해당 속성명으로 받으면된다!
+  return (
+    <figure>
+        <img src={"./images/" + props.isrc} alt={props.ialt} />
+        <figcaption>{props.ialt}</figcaption>
+    </figure>
+
+  );
+  
+} ////////// MakeImage 컴포넌트 ///////
