17d645a8335cf1c505d77be1dbfcec62e31cdf26
Tom B. Parker <tombap8@hotmail.com>
2025-02-19 15:32:00 +0900

    ss

--- 008.React학습/04.react-app/02.리액트소스/tom-app/src/index.js
+++ 008.React학습/04.react-app/02.리액트소스/tom-app/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+// 루트요소 선택하기 ///
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 루트에 랜더링하기 ////
+root.render(
+  <>
+    <h1>
+      <b>선재업고</b>
+      <span>튀어</span>
+    </h1>
+  </>
+);
