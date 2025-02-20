8c1aec0d2d3d114d2fad7f0491b26ee2e04b65b3
Tom B. Parker <tombap8@hotmail.com>
2025-02-20 16:39:06 +0900

    ss

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -0,0 +1,12 @@
+/// 상단영역 컴포넌트 : TopArea.jsx /////
+
+export default function TopArea(){
+
+    /// 리턴 코드구역 ////////
+    return (
+        <>
+        
+        </>
+    );
+
+} //////////// TopArea 컴포넌트 ///////////
