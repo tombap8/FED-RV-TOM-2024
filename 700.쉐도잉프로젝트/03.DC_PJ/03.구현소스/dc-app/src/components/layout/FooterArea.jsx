54c11f7db494c947fc4bf891772491c0e8704c52
Tom B. Parker <tombap8@hotmail.com>
2025-02-21 15:50:28 +0900

    Update FooterArea.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/FooterArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/FooterArea.jsx
@@ -1,15 +1,23 @@
 /// 하단영역 컴포넌트 : FooterArea.jsx /////
 
 // 하단메뉴 데이터 불러오기 ////
-import { bmData } from '../../js/data/bmenu'
+import { bmData } from "../../js/data/bmenu";
 
-export default function FooterArea(){
-
-    /// 리턴 코드구역 ////////
-    return (
-        <>
-            <h1>하단영역 컴포넌트</h1>
-        </>
-    );
-
+export default function FooterArea() {
+  /// 리턴 코드구역 ////////
+  return (
+    <footer className="info">
+      <ul>
+        {/* 하단로고 컴포넌트 넣기 */}
+        <li>
+          <Logo logoStyle="bottom" />
+        </li>
+        <li>
+          {/* 하단링크박스 */}
+          <ol className="bmenu"></ol>
+        </li>
+        <li>© & ™ DC. ALL RIGHTS RESERVED</li>
+      </ul>
+    </footer>
+  );
 } //////////// FooterArea 컴포넌트 ///////////
