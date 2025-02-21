624517f45ce336602b93fdc2922c50c1168011da
Tom B. Parker <tombap8@hotmail.com>
2025-02-21 12:57:49 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -1,12 +1,23 @@
 /// 상단영역 컴포넌트 : TopArea.jsx /////
 
-export default function TopArea(){
-
-    /// 리턴 코드구역 ////////
-    return (
-        <>
-            <h1>상단영역 컴포넌트</h1>
-        </>
-    );
-
+export default function TopArea() {
+  /// 리턴 코드구역 ////////
+  return (
+    <>
+      {/* 1.상단영역 */}
+      <header className="top-area">
+        {/* 로그인 환영메시지 박스 */}
+        <div className="logmsg"></div>
+        {/* 네비게이션 GNB파트 */}
+        <nav className="gnb">
+          <ul>
+            {/* 1. 로고 컴포넌트 */}
+            <li></li>
+          </ul>
+        </nav>
+        {/* 모바일용 햄버거 버튼 */}
+        <button className="hambtn"></button>
+      </header>
+    </>
+  );
 } //////////// TopArea 컴포넌트 ///////////
