fd6bdd92f496c27cdd33ba61d3c8bc3f5ba0b495
Tom B. Parker <tombap8@hotmail.com>
2025-02-24 13:05:22 +0900

    Update 01.공유신발.jsx

--- 008.React학습/02.react-ex/js/01.공유신발.jsx
+++ 008.React학습/02.react-ex/js/01.공유신발.jsx
@@ -22,54 +22,64 @@ import * as comFn from "./common/com_fn";
 // 함수형 컴포넌트는 첫글자 대문자인 함수키워드로 만든다
 
 function MainComponent() {
-    // [ 후크 상태관리 변수 셋팅!!! ]
-    // 1. 리스트 / 상세보기 전환용 상태관리변수
-    const [ viewList, setViewList ] = React.useState(true);
-    // 2. 상품 데이터 인덱스값 상태관리변수
-    const [ gIdx, setGIdx ] = React.useState(1);
-    // 3. 선택 아이템 고유이름 상태관리변수
-    const [ selItem, setSelItem ] = React.useState("공유");
-    // 4. 테스트용 상태관리변수(의존성 테스트용!)
-    const [ test, setTest ] = React.useState(true);
+  // [ 후크 상태관리 변수 셋팅!!! ]
+  // 1. 리스트 / 상세보기 전환용 상태관리변수
+  const [viewList, setViewList] = React.useState(true);
+  // 2. 상품 데이터 인덱스값 상태관리변수
+  const [gIdx, setGIdx] = React.useState(1);
+  // 3. 선택 아이템 고유이름 상태관리변수 : 값(공유/효진)
+  const [selItem, setSelItem] = React.useState("공유");
+  // 4. 테스트용 상태관리변수(의존성 테스트용!)
+  const [test, setTest] = React.useState(true);
 
-    // [ useEffect 테스트 함수 ] ///
-    const testFn = () => {
-      // 의존성 테스트를 위한 상태변수 업데이트
-      setTest(!test); 
-      // -> 이 함수를 호출하면 true/false값이 반대로 셋팅됨!
-      console.log("테스트중~! 변경할값:",!test);
-      console.log("리랜더링 전 test상태변수값:",test);
+  // [ useEffect 테스트 함수 ] ///
+  const testFn = () => {
+    // 의존성 테스트를 위한 상태변수 업데이트
+    setTest(!test);
+    // -> 이 함수를 호출하면 true/false값이 반대로 셋팅됨!
+    console.log("테스트중~! 변경할값:", !test);
+    console.log("리랜더링 전 test상태변수값:", test);
+  }; ///////////// testFn ////////////////
 
-    }; ///////////// testFn ////////////////
+  // [ 1. useEffect : 의존성이없는 경우 ] ///
+  // -> 컴포넌트가 생성, 변경, 삭제전 DOM을 랜더링하면
+  // 매번 실행되는 코드구역이다!!!
+  React.useEffect(() => {
+    console.log("DOM이 완성되었어!");
+    console.log("🍜랜더링후 test상태변수값:", test);
+  }); ////////////// useEffect ///////////////
 
-    // [ 1. useEffect : 의존성이없는 경우 ] ///
-    // -> 컴포넌트가 생성, 변경, 삭제전 DOM을 랜더링하면
-    // 매번 실행되는 코드구역이다!!!
-    React.useEffect(()=>{
-      console.log("DOM이 완성되었어!");
-      console.log("🍜랜더링후 test상태변수값:",test);
-    }); ////////////// useEffect ///////////////
+  // [ 2. useEffect : 의존성이있는 경우 ] ///
+  React.useEffect(() => {
+    console.log("의존성useEffect실행![test]");
+  }, [test]);
+  // 의존성이란? useEffect가 실행되는 것에 관련된
+  // 상태변수를 등록하여 실행구역을 컨트롤한다!
+  // 즉, 등록된 상태변수가 변경될때만 이 구역은 실행된다!
+  // 의존성등록은 이렇게한다!
+  // -> useEffect(함수,[의존성변수])
+  // -> 함수 뒤에 콤마후 배열형으로 넣는다
+  // -> 배열형이므로 여러개를 등록할 수 있다!
 
-    
-    // [ 2. useEffect : 의존성이있는 경우 ] ///
-    React.useEffect(()=>{
-      console.log("의존성useEffect실행![test]");
-    },[test]);
-    // 의존성이란? useEffect가 실행되는 것에 관련된
-    // 상태변수를 등록하여 실행구역을 컨트롤한다!
-    // 즉, 등록된 상태변수가 변경될때만 이 구역은 실행된다!
-    // 의존성등록은 이렇게한다!
-    // -> useEffect(함수,[의존성변수])
-    // -> 함수 뒤에 콤마후 배열형으로 넣는다
-    // -> 배열형이므로 여러개를 등록할 수 있다!
+  // [ 3. useEffect : 의존성이있으나 빈 경우 ] ///
+  React.useEffect(() => {
+    console.log("useEffect 의존성이 비어서 한번만 실행!");
+  }, []);
+  // -> useEffect(함수,[])
+  // -> 최초로딩시 한번만 실행한다!
 
-    // [ 3. useEffect : 의존성이있으나 빈 경우 ] ///
-    React.useEffect(()=>{
-      console.log("useEffect 의존성이 비어서 한번만 실행!");
-    },[]);
-    // -> useEffect(함수,[])
-    // -> 최초로딩시 한번만 실행한다!
+  // [ 4. useLayoutEffect :
+  // 화면랜더링 전 DOM완성후 실행구역 ] ///
+  React.useLayoutEffect(() => {
+    console.log("화면랜더링전 DOM완성후 실행!");
+  }, [test]); // -> 의존성실행!
+  // },[]); -> 최초한번실행
+  // }); -> 매번실행
 
+  // -> useEffect보다 먼저 실행됨!
+  // -> useLayoutEffect도 의존성 셋팅은 useEffect와 동일함!
+  // useLayoutEffect(함수,[의존성])
+  // useLayoutEffect(함수,[]) -> 최초한번만 실행
 
   /************************************** 
     [ 코드구성 ]
@@ -111,43 +121,59 @@ function MainComponent() {
             marginRight: "10px",
           }}
         />
-        <span>공유가 신고 다닌다는!</span>
+        <span>{
+        selItem + 
+        (selItem==="공유"?
+          "가 신고 다닌다는!":
+          "이 입고 다닌다는!")
+          }</span>
       </h1>
       <section>
-        <h2 className="stit">공유는 오늘도 멋찝니다!</h2>
+        <h2 className="stit">
+          {
+            selItem === "공유"?
+            "공유는 오늘도 멋찝니다!":
+            "효진은 오늘도 쨍~합니다!"
+          }          
+          </h2>
         <div className="img-box">
           <img src="./images/vans/gongyoo.jpg" alt="멋진공유" />
         </div>
       </section>
       <div className="btn-box">
-        <button>효진초이스 바로가기</button>
-        <br />
         <button
-          onClick={testFn}
-        >useEffect 의존성 테스트</button>
+          onClick={() => {
+            // 선택 아이템 변경하기
+            setSelItem(selItem === "공유" ? "효진" : "공유");
+          }}
+        >
+          {selItem === "공유" ? "효진" : "공유"}초이스 바로가기
+        </button>
+        <br />
+        <button onClick={testFn}>useEffect 의존성 테스트</button>
       </div>
       <div className="gwrap">
         {
-            // 상태변수 viewList가 true면
-            // 상품리스트 하위 컴포넌트 보이기
-            viewList ? 
-            <GoodsList 
-                selItem={selItem} 
-                // 상태변수업데이트를 위해 자식에게 
-                // 변수 업데이트 메서드를 보내준다!
-                setGIdx={setGIdx}
-                setViewList={setViewList}
+          // 상태변수 viewList가 true면
+          // 상품리스트 하위 컴포넌트 보이기
+          viewList ? (
+            <GoodsList
+              selItem={selItem}
+              // 상태변수업데이트를 위해 자식에게
+              // 변수 업데이트 메서드를 보내준다!
+              setGIdx={setGIdx}
+              setViewList={setViewList}
             />
-            :
+          ) : (
             // 상태변수 viewList가 false면
             // 상품상세보기 하위 컴포넌트 보이기
-            <GoodsDetail 
-                selItem={selItem} 
-                gIdx={gIdx}            
-                setViewList={setViewList}
+            <GoodsDetail
+              selItem={selItem}
+              gIdx={gIdx}
+              setViewList={setViewList}
             />
+          )
         }
-
       </div>
     </React.Fragment>
   );
@@ -157,4 +183,3 @@ function MainComponent() {
 ReactDOM.render(<MainComponent />, document.querySelector("#root"));
 // ReactDOM.render(어쩌구,저쩌구);
 // 어쩌구를 저쩌구에 출력해라!
-
