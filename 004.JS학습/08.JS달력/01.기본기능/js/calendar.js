c8484f055430303b6687023c10f477d93a253c84
Tom B. Parker <tombap8@hotmail.com>
2025-02-25 11:21:50 +0900

    Update calendar.js

--- 004.JS학습/08.JS달력/01.기본기능/js/calendar.js
+++ 004.JS학습/08.JS달력/01.기본기능/js/calendar.js
@@ -23,5 +23,29 @@ makeDallyeok();
 
 function makeDallyeok(){
     myFn.cs('달력만들어!');
+    
+
+    // 1. 변수셋팅 ////////////////////
+    // (1) 변경할 현재날짜 객체
+    const currDate = new Date();
+    // (2) 오늘날짜 객체
+    const today = new Date();
+    // (3) 년도요소 : .yearTit
+    const yearTit = myFn.qs('.yearTit');
+    // (4) 월요소 : .monthTit
+    const monthTit = myFn.qs('.monthTit');
+    // (5) 날짜요소 : .dates
+    const dates = myFn.qs('.dates');
+    // (6) 날짜넣을 배열변수
+    const dateSet = [];
+    // (7) html 코드 저장변수
+    let hcode = '';
+
+    // 2. 함수 만들기 //////////////
+    // (1) 달력 초기화 구성 함수 ////
+    const initDallyeok = () => {
+
+    }; //////////// initDallyeok 함수 ///////
+
 
 } /////////////// makeDallyeok함수 ////////////
