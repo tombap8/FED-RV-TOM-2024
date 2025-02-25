e1b4fcf0b638398fffee9bf5f7e9deaf62ed7fd8
Tom B. Parker <tombap8@hotmail.com>
2025-02-25 15:40:56 +0900

    Update News.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/pages/News.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/pages/News.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../modules/Banner';
 
 function News(props) {
     return (
-        <div>
-            <h1>뉴스야!</h1>
-        </div>
+        <>
+          {/* 1. 배너 컴포넌트 */}
+          <Banner catName="NEWS" />
+        </>
     );
 }
 
