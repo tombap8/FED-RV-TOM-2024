f63721930e9318d081cb9574a19ca41f6bb99dc7
Tom B. Parker <tombap8@hotmail.com>
2025-02-25 11:45:36 +0900

    Update calendar.js

--- 004.JS학습/08.JS달력/01.기본기능/js/calendar.js
+++ 004.JS학습/08.JS달력/01.기본기능/js/calendar.js
@@ -2,68 +2,69 @@
 
 // DOM 메서드 //////
 const myFn = {
-    qs : x => document.querySelector(x),
-    qsa : x => document.querySelectorAll(x),
-    qsEl: (el, x) => el.querySelector(x),
-    qsaEl: (el, x) => el.querySelectorAll(x),
-    addEvt : (ele,evt,fn) => 
-            ele.addEventListener(evt,fn),
-    cs : x => console.log(x),
-    addZero : x => x < 10 ? '0' + x : x,
-    dfm : x => `${x.getFullYear()}-${
-        dFn.addZero(x.getMonth()+1)}-${
-        dFn.addZero(x.getDate())}(${week[x.getDay()]})`
+  qs: (x) => document.querySelector(x),
+  qsa: (x) => document.querySelectorAll(x),
+  qsEl: (el, x) => el.querySelector(x),
+  qsaEl: (el, x) => el.querySelectorAll(x),
+  addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+  cs: (x) => console.log(x),
+  addZero: (x) => (x < 10 ? "0" + x : x),
+  dfm: (x) =>
+    `${x.getFullYear()}-${myFn.addZero(x.getMonth() + 1)}-${myFn.addZero(
+      x.getDate()
+    )}(${week[x.getDay()]})`,
 }; ///////// dFn 객체 //////////
-    
+
 // 요일변경배열 ////
-const week = ["일","월","화","수","목","금","토"];
+const week = ["일", "월", "화", "수", "목", "금", "토"];
 
 // 달력함수 호출
 makeDallyeok();
 
-function makeDallyeok(){
-    myFn.cs('달력만들어!');
-    
+function makeDallyeok() {
+  myFn.cs("달력만들어!");
 
-    // 1. 변수셋팅 ////////////////////
-    // (1) 변경할 현재날짜 객체
-    const currDate = new Date();
-    // (2) 오늘날짜 객체
-    const today = new Date();
-    // (3) 년도요소 : .yearTit
-    const yearTit = myFn.qs('.yearTit');
-    // (4) 월요소 : .monthTit
-    const monthTit = myFn.qs('.monthTit');
-    // (5) 날짜요소 : .dates
-    const dates = myFn.qs('.dates');
-    // (6) 날짜넣을 배열변수
-    const dateSet = [];
-    // (7) html 코드 저장변수
-    let hcode = '';
+  // 1. 변수셋팅 ////////////////////
+  // (1) 변경할 현재날짜 객체
+  const currDate = new Date();
+  // (2) 오늘날짜 객체
+  const today = new Date();
+  // (3) 년도요소 : .yearTit
+  const yearTit = myFn.qs(".yearTit");
+  // (4) 월요소 : .monthTit
+  const monthTit = myFn.qs(".monthTit");
+  // (5) 날짜요소 : .dates
+  const dates = myFn.qs(".dates");
+  // (6) 날짜넣을 배열변수
+  const dateSet = [];
+  // (7) html 코드 저장변수
+  let hcode = "";
 
-    // 2. 함수 만들기 //////////////
-    // (1) 달력 초기화 구성 함수 ////
-    const initDallyeok = () => {
+  // 2. 함수 만들기 //////////////
+  // (1) 달력 초기화 구성 함수 ////
+  const initDallyeok = () => {
+    // 1) 변수초기화
 
-        // 1) 변수초기화
+    // 2) 변수할당 //////////
+    // 현재년
+    let cYr = currDate.getFullYear();
 
-        // 2) 변수할당 //////////
-        // 현재년
-        let cYr = currDate.getFullYear();
+    // 현재달 : 배열순번
+    let cMt = currDate.getMonth();
 
-        // 현재달 : 배열순번
-        let cMt = currDate.getMonth();
-        
-        // [선택달  끝날짜, 첫날짜 알아오기]
-        // new Date(년도,월,옵션)
-        // (1) 년도
-        // (2) 월
-        // (3) 옵션 : 0 - 선택달끝날짜 / 1 - 다음달첫날짜
+    // [선택달  끝날짜, 첫날짜 알아오기]
+    // new Date(년도,월,옵션)
+    // (1) 년도
+    // (2) 월
+    // (3) 옵션 : 0 - 이전달끝날짜 / 1 - 현재달첫날짜
 
-    }; //////////// initDallyeok 함수 ///////
+    // 3) 날짜 데이터 셋업 //////
+    // [1] 전달 마지막 날짜(옵션:0)
+    const prevLast = new Date(cYr, cMt, 0);
 
-    // 초기셋팅함수 호출!
-    initDallyeok();
+    myFn.cs("전달 마지막 날짜:" + myFn.dfm(prevLast));
+  }; //////////// initDallyeok 함수 ///////
 
-
+  // 초기셋팅함수 호출!
+  initDallyeok();
 } /////////////// makeDallyeok함수 ////////////
