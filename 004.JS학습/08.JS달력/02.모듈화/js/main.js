aedc31557dc5bfb43d0380b1bea1d7beb4d4f14e
Tom B. Parker <tombap8@hotmail.com>
2025-02-26 13:16:39 +0900

    Update main.js

--- 004.JS학습/08.JS달력/02.모듈화/js/main.js
+++ 004.JS학습/08.JS달력/02.모듈화/js/main.js
@@ -2,4 +2,9 @@
 
 // 달력 컴포넌트 불러오기
 import MakeDallyeok from "./CalendarComp.js";
-console.log(MakeDallyeok);
+// console.log(MakeDallyeok);
+
+// 달력 컴포넌트 함수 호출하여 달력 셋팅하기!
+// 대상: .dalcom1, .dalcom2
+const dc1 = new MakeDallyeok('.dalcom1');
+const dc2 = new MakeDallyeok('.dalcom2');
