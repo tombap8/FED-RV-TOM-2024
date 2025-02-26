c6521fab4df1733ec7a3832fd5cb25b342cfef3d
Tom B. Parker <tombap8@hotmail.com>
2025-02-26 16:43:19 +0900

    Update SecIntro.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/SecIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/SecIntro.jsx
@@ -1,77 +1,40 @@
 // DC.com 섹션소개 컴포넌트 - SecIntro.jsx ///
 import React from "react";
 
+// 섹션 소개 모듈용 데이터 불러오기
+import { secIntroData } from "../../js/data/sec_intro";
+
+// 섹션 소개 모듈용 CSS 불러오기
+import "../../css/modules/sec_intro.scss";
+import { Link } from "react-router-dom";
+
 function SecIntro(props) {
+  // 불러온 데이터 변수할당 ////
+  const selData = secIntroData;
+
+  // 리턴 코드구역 ///////////
   return (
     <section className="sec-intro">
-      <div>
-        <div className="imbx">
-          <img
-            src="/FED-RF-2nd-2024-TOM/700.%EC%89%90%EB%8F%84%EC%9E%89%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.DC_PJ/02.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/dc-app/build/images/menubtn1.jpg"
-            alt="WORLDS COLLIDE"
-          />
+      {/* 반복단위박스 */}
+      {selData.map((v, i) => (
+        <div key={i}>
+          <div className="imbx">
+            <img
+              src={v.isrc}
+              alt={v.tit.split('^')[0]}
+            />
+          </div>
+          <div className="titbx">
+            <h3>{v.tit.split('^')[0]}</h3>
+            <h2>{v.tit.split('^')[1]}</h2>
+          </div>
+          <div className="btnbx">
+            <Link to={v.link}>
+              <button>{v.btn}</button>
+            </Link>           
+          </div>
         </div>
-        <div className="titbx">
-          <h3>WORLDS COLLIDE</h3>
-          <h2>The Flash is Only in Theaters June 16</h2>
-        </div>
-        <div className="btnbx">
-          <a href="/FED-RF-2nd-2024-TOM/700.%EC%89%90%EB%8F%84%EC%9E%89%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.DC_PJ/02.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/dc-app/build/character">
-            <button>TICKETS ON SALE NOW</button>
-          </a>
-        </div>
-      </div>
-      <div>
-        <div className="imbx">
-          <img
-            src="/FED-RF-2nd-2024-TOM/700.%EC%89%90%EB%8F%84%EC%9E%89%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.DC_PJ/02.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/dc-app/build/images/menubtn2.jpg"
-            alt="DC SHOP"
-          />
-        </div>
-        <div className="titbx">
-          <h3>DC SHOP</h3>
-          <h2>GET NUTS WITH THE 1/6 SCALE BATMAN FIGURE</h2>
-        </div>
-        <div className="btnbx">
-          <a href="/FED-RF-2nd-2024-TOM/700.%EC%89%90%EB%8F%84%EC%9E%89%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.DC_PJ/02.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/dc-app/build/comics">
-            <button>Shop New Arrivals</button>
-          </a>
-        </div>
-      </div>
-      <div>
-        <div className="imbx">
-          <img
-            src="/FED-RF-2nd-2024-TOM/700.%EC%89%90%EB%8F%84%EC%9E%89%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.DC_PJ/02.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/dc-app/build/images/menubtn3.jpg"
-            alt="DC PRIDE"
-          />
-        </div>
-        <div className="titbx">
-          <h3>DC PRIDE</h3>
-          <h2>Celebrate with LGBTQ creators, characters, &amp; stories</h2>
-        </div>
-        <div className="btnbx">
-          <a href="/FED-RF-2nd-2024-TOM/700.%EC%89%90%EB%8F%84%EC%9E%89%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.DC_PJ/02.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/dc-app/build/movies">
-            <button>Visit our Pride Hub</button>
-          </a>
-        </div>
-      </div>
-      <div>
-        <div className="imbx">
-          <img
-            src="/FED-RF-2nd-2024-TOM/700.%EC%89%90%EB%8F%84%EC%9E%89%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.DC_PJ/02.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/dc-app/build/images/menubtn4.jpg"
-            alt="DC COMMUNITY "
-          />
-        </div>
-        <div className="titbx">
-          <h3>DC COMMUNITY </h3>
-          <h2>Share Your DC Pride Fanworks</h2>
-        </div>
-        <div className="btnbx">
-          <a href="/FED-RF-2nd-2024-TOM/700.%EC%89%90%EB%8F%84%EC%9E%89%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/03.DC_PJ/02.%EA%B5%AC%ED%98%84%EC%86%8C%EC%8A%A4/dc-app/build/games">
-            <button>SUBMIT YOUR ART</button>
-          </a>
-        </div>
-      </div>
+      ))}
     </section>
   );
 }
