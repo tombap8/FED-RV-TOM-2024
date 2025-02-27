2259a688aadd5b0972ad6e5859d602a485649907
Tom B. Parker <tombap8@hotmail.com>
2025-02-27 11:09:37 +0900

    ss

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/VidIntro.jsx
@@ -0,0 +1,31 @@
+// DC.com 비디오소개 컴포넌트 - VidIntro.jsx ///
+import React from "react";
+
+function VidIntro(props) {
+  return (
+    <section className="off vidbox">
+      <div>
+        <div className="vb1">
+          <iframe
+            src="https://www.youtube.com/embed/jprhe-cWKGs"
+            title="THE FLASH - FINAL TRAILER"
+          ></iframe>
+        </div>
+        <div className="vb2">
+          <h3>FEATURED VIDEO</h3>
+          <h2>THE FLASH - FINAL TRAILER</h2>
+          <p>Are YOU ready? THE FLASH - Only in Theaters June 16.</p>
+          <p>
+            Advance tickets available now!{" "}
+            <a href="https://www.dc.com/theflash" target="_blank">
+              Visit our official Flash
+            </a>{" "}
+            hub for tickets and showtimes!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default VidIntro;
