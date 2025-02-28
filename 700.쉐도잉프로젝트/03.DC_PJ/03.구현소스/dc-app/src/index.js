1bffe2a67ad4e4b3a532c6575841afb4ef8e5d7c
Tom B. Parker <tombap8@hotmail.com>
2025-02-28 11:20:13 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/index.js
@@ -14,6 +14,7 @@ import Games from './components/pages/Games';
 import News from './components/pages/News';
 import Video from './components/pages/Video';
 import Board from './components/pages/Board';
+import SwiperApp from './components/plugin/SwiperApp';
 // 사스파일에서 import시엔 _와 .scss생략가능하나
 // 리액트 import에서는 모두 정확히 써야함!
 
@@ -63,7 +64,7 @@ export default function MainComponent(){
                 슬래쉬는 루트를 말하고 레이아웃 컴포넌트 불러옴 */}
                 <Route path="/" element={<Layout />}>
                 {/* 하위중 첫페이지는 index라고 속성씀! */}
-                    <Route index element={<Main catName="main" />}  />
+                    {/* <Route index element={<Main catName="main" />}  /> */}
                     <Route path='character' element={<Character />}  />
                     <Route path='comics' element={<Comics catName="COMICS" />}  />
                     <Route path='movies' element={<Movies catName="MOVIES" />}  />
@@ -71,6 +72,7 @@ export default function MainComponent(){
                     <Route path='news' element={<News />}  />
                     <Route path='video' element={<Video catName="VIDEO" />}  />
                     <Route path='board' element={<Board />}  />
+                    <Route index element={<SwiperApp />}  />
                 </Route>
             </Routes>
         </BrowserRouter>
