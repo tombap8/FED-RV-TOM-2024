adac2f55b6b0c88709dcf64c474a88d7d71796ca
Tom B. Parker <tombap8@hotmail.com>
2025-02-28 13:08:37 +0900

    Update SwiperVid.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
@@ -9,57 +9,72 @@ import "swiper/css";
 import "swiper/css/navigation";
 // import "swiper/css/pagination";
 
+// 스와이퍼 비디오 모듈 CSS 불러오기
 import "./css/swiper_vid.scss";
+
+// 스와이퍼 비디오 모듈 데이터 불러오기
+import { swVidData } from "../../js/data/swiper_vid";
 
 // import required modules
 // 사용할 스와이퍼 모듈을 불러온다!
-// (여기서는 페이지네이션, 네비게이션,자동넘김)
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+// (여기서는 네비게이션)
+import { Navigation } from "swiper/modules";
 
-export default function SwiperVid() {
-  // 불러올 이미지 리스트 //
-  const imgArr = [
-    "dcm28",
-    "dcm29",
-    "dcm30",
-    "dcm31",
-    "dcm32",
-    "dcm10",
-    "dcm11",
-    "dcm12",
-  ];
+export default function SwiperVid({ catName }) {
+  // catName - 데이터 선택용 분류명
+
+  // 선택 데이터 변수할당
+  const selData = swVidData["main"];
+  console.log(selData);
 
   // 리턴 코드구역 /////////
   return (
     <>
       <Swiper
         // 한번에 보여줄 슬라이드수
-        slidesPerView={3}
+        slidesPerView={4}
         // 슬라이드 사이 간격(단위없는 px수)
-        spaceBetween={30}
+        spaceBetween={20}
         // 양쪽이동 네이게이션 사용여부(모듈import필수!)
         navigation={true}
-        // 블릿표시 페이지네이션 사용여부(모듈import필수!)
-        pagination={{
-          clickable: true, //블릿클릭이동옵션
-        }}
-        // 자동넘김 기능 모듈 셋팅
-        autoplay={{
-          delay: 2500, // 대기시간 2.5초
-          disableOnInteraction: false, // 인터렉션 없앰속성 false
-          // -> 인터렉션을 살려놔야 터치후 다시 자동넘김 작동함!
+        // 스와이퍼 사이즈별 슬라이드 수 변경하기
+        breakpoints={{
+          200: {
+            slidesPerView: 1,
+          },
+          500: {
+            slidesPerView: 2,
+          },
+          1000: {
+            slidesPerView: 3,
+          },
+          1200: {
+            slidesPerView: 4,
+          },
         }}
         // ★스와이퍼 추가사용 모듈 등록 속성(사용시 등록필수!)
-        modules={[Navigation, Pagination, Autoplay]}
-        // 무한이동슬라이드 설정
-        loop={true}
+        modules={[Navigation]}
         className="mySwiper"
       >
         {
-          // 배열이미지 이름만큼 반복하여 슬라이드 생성!
-          imgArr.map((v, i) => (
+          // 배열 데이터만큼 반복하여 슬라이드 생성!
+          selData.map((v, i) => (
             <SwiperSlide key={i}>
-              <img src={"./images/" + v + ".jpg"} alt="list image" />
+              <section className="sw-inbox">
+                {/* 동영상 이미지박스 */}
+                <div className="vid-img">
+                  <img
+                    src={v.isrc}
+                    alt={v.tit}
+                  />
+                  {/* 폰트어썸 아이콘 */}
+                </div>
+                {/* 동영상 타이틀 박스 */}
+                <div className="vid-tit">
+                  <h4>{v.cat}</h4>
+                  <h3>{v.tit}</h3>
+                </div>
+              </section>
             </SwiperSlide>
           ))
         }
