fa7494cde4ba3ac509aa4a06799270648651a6be
Tom B. Parker <tombap8@hotmail.com>
2025-02-28 16:43:37 +0900

    Update TopArea.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/layout/TopArea.jsx
@@ -1,12 +1,36 @@
 /// 상단영역 컴포넌트 : TopArea.jsx /////
 
-import { Link } from "react-router-dom";
+// GNB 데이터 가져오기 ////
+import { gnbData } from "../../js/data/gnb";
+
+// 전체메뉴 컴포넌트 불러오기
 
 export default function TopArea() {
   /// 리턴 코드구역 ////////
   return (
     <>
-      <h1>상단영역</h1>
+      <div id="top-area">
+        <header className="top-area ibx">
+          <h1 id="logo">
+            <a href="#">
+              <img
+                src={process.env.PUBLIC_URL + "/images/main_logo.png"}
+                alt="파일럿로고"
+              />
+            </a>
+          </h1>
+          <nav className="gnb">
+            <ul>
+              <li className="bld">배너순번 li 숨기기</li>
+              {makeList(props.cat)}
+            </ul>
+          </nav>
+          <div className="ham">
+            <span></span> <span></span> <span></span>
+          </div>
+          {/* 전체메뉴 컴포넌트 */}
+        </header>
+      </div>
     </>
   );
 } //////////// TopArea 컴포넌트 ///////////
