9eb3bf782899e18e72bcbbc88b3cf1fdf81b311d
Tom B. Parker <tombap8@hotmail.com>
2025-03-04 13:01:56 +0900

    Update CatDetail.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/pages/CatDetail.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/pages/CatDetail.jsx
@@ -4,47 +4,41 @@ import React from "react";
 import Banner from "../modules/Banner";
 import CatList from "../modules/CatList";
 
-// 모듈 CSS 불러오기 
-import '../../css/pages/cat_detail.scss';
+// 모듈 CSS 불러오기
+import "../../css/pages/cat_detail.scss";
+import { useLocation } from "react-router-dom";
 
-function CatDetail(props) {
+function CatDetail() {
+  // 라우터 호출시 전달한 값을
+  // useLocation 인스턴스 중 state속성을 같은
+  // 이름의 변수로 구조분해할당 방식으로 받는다!
+  const { state } = useLocation();
+  // state객체의 같은 이름의 속성명으로 구조분해할당한다!
+  const { cname, cdesc, facts } = state;
+  // console.log(cname,cdesc,facts);
+  // 전달된 변수
+  // 1. cname - 캐릭터이름(배너 catName 속성정보로도 사용)
+  // 2. cdesc - 캐릭터 설명
+  // 3. facts - 캐릭터 상세
+
+  // 리턴 코드구역 ///////////////
   return (
     <>
       {/* 1. 배너모듈 */}
-      <Banner catName="BATMAN" />
+      <Banner catName={cname} />
       {/* 2. 상세정보박스 */}
       <div className="detail">
         {/* 2-1. 캐릭터 설명박스 */}
         <div className="desc-box">
           {/* (1) 캐릭터명 */}
-          <h2>BATMAN</h2>
+          <h2>{cname}</h2>
           {/* (2) 캐릭터소개 */}
           <div className="cdesc">
-            <p>
-              In the name of his murdered parents, Bruce Wayne wages eternal war
-              on the criminals of Gotham City. He is vengeance. He is the night.
-              He is Batman.
-            </p>
-            <p>
-              One of the most iconic fictional characters in the world, Batman
-              has dedicated his life to an endless crusade, a war on all
-              criminals in the name of his murdered parents, who were taken from
-              him when he was just a child. Since that tragic night, he has
-              trained his body and mind to near physical perfection to be a
-              self-made Super Hero. He's developed an arsenal of technology that
-              would put most armies to shame. And he's assembled teams of his
-              fellow DC Super Heroes, like the Justice League, the Outsiders and
-              Batman, Incorporated.
-            </p>
-            <p>
-              A playboy billionaire by day, Bruce Wayne’s double life affords
-              him the comfort of a life without financial worry, a loyal
-              butler-turned-guardian and the perfect base of operations in the
-              ancient network of caves beneath his family’s sprawling estate. By
-              night, however, he sheds all pretense, dons his iconic scalloped
-              cape and pointed cowl and takes to the shadowy streets, skies and
-              rooftops of Gotham City.
-            </p>
+            {
+                // cdesc 데이터의 '^'로 잘라서 p요소 할당
+                cdesc.split('^').map((v,i)=>
+                <p key={i}>{v}</p>)                
+            }
           </div>
         </div>
         {/* 2-2. 캐릭터명세 */}
