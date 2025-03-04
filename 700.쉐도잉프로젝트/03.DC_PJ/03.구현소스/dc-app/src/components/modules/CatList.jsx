e256d064f20148416305d766dc4dd349cd4891bf
Tom B. Parker <tombap8@hotmail.com>
2025-03-04 11:00:47 +0900

    ss

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/CatList.jsx
@@ -0,0 +1,17 @@
+// DC.com 캐릭터 리스트 컴포넌트 - CatList.jsx
+
+import React from 'react';
+
+function CatList(props) {
+    return (
+        <section className='cat-swbox'>
+            {/* 1. 모듈 타이틀 */}
+            <h2 className="tit">
+                WHO'S WHO: THE JUSTICE LEAGUE
+            </h2>
+            {/* 2. 스와이퍼 컴포넌트 */}
+        </section>
+    );
+}
+
+export default CatList;
