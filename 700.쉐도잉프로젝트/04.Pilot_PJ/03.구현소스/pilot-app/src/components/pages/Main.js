cbdfdc2c4c05a86272a8722bb4c191bc8ff4cf7c
Tom B. Parker <tombap8@hotmail.com>
2025-03-04 16:01:16 +0900

    Update Main.js

--- 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/Main.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/Main.js
@@ -9,6 +9,7 @@ import * as autoFn from "../../js/func/jquery-autoScroll";
 import { dragBanner } from "../../js/func/drag_banner";
 
 import { Banner } from "../modules/Banner";
+import { FashionIntro } from "../modules/FashionIntro";
 
 function Main() {
     // 컴포넌트 로딩후 실행구역 : 한번만 (빈의존성[]) /////
@@ -19,6 +20,16 @@ function Main() {
         // -> window 이벤트 설정을 여기서한 이유는?
         // ->>> 소멸자를 통해 이벤트를 다른 페이지가 나올때
         // 해재해 주기 위해 여기서 셋팅함!
+
+        // 메뉴+인디케이터 이벤트 기능 설정함수 호출 ///
+        autoFn.evtFn();
+
+        // 초기화 함수 호출
+        autoFn.initSet();
+
+        // 페이지번호 초기화 함수 호출
+        autoFn.zeroPno();
+
 
         // 드래그배너 기능함수 호출하기
         dragBanner();
@@ -39,13 +50,19 @@ function Main() {
       </section>
 
       {/* 2. 남성패션 페이지 */}
-      <section className="page"></section>
+      <section className="page">
+        <FashionIntro cat="men" subcat="etc" />
+      </section>
 
       {/* 3. 여성패션 페이지 */}
-      <section className="page"></section>
+      <section className="page">
+        <FashionIntro cat="women" subcat="etc" />
+      </section>
 
       {/* 4. 스타일패션 페이지 */}
-      <section className="page"></section>
+      <section className="page">
+        <FashionIntro cat="style" subcat="etc" />
+      </section>
 
       {/* 메인에만 나오는 사이드 인디케이터 */}
       <nav className="indic">
