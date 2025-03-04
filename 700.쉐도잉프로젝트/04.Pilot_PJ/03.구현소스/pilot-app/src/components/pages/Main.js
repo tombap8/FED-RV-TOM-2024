2179e5a050ae24e970416684bab5698d9180c9d2
Tom B. Parker <tombap8@hotmail.com>
2025-03-04 15:18:18 +0900

    Update Main.js

--- 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/Main.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/03.구현소스/pilot-app/src/components/pages/Main.js
@@ -11,6 +11,41 @@ function Main(props) {
         className="page none-sel"
         style={{ background: "lightblue" }}
       ></section>
+
+      {/* 2. 남성패션 페이지 */}
+      <section className="page"></section>
+
+      {/* 3. 여성패션 페이지 */}
+      <section className="page"></section>
+
+      {/* 4. 스타일패션 페이지 */}
+      <section className="page"></section>
+
+      {/* 메인에만 나오는 사이드 인디케이터 */}
+      <nav className="indic">
+        <ul>
+          <li className="on">
+            <a href="#ban">
+              <span>배너</span>
+            </a>
+          </li>
+          <li>
+            <a href="#men">
+              <span>남성의류</span>
+            </a>
+          </li>
+          <li>
+            <a href="#women">
+              <span>여성의류</span>
+            </a>
+          </li>
+          <li>
+            <a href="#style">
+              <span>스타일</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
