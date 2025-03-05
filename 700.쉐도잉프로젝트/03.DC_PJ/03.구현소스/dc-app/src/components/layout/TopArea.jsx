770385ef1a4881226ba5ce0421555d76ead72821
Tom B. Parker <tombap8@hotmail.com>
2025-03-05 11:20:37 +0900

    Update TopArea.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -9,7 +9,29 @@ import { menu } from "../../js/data/gnb";
 import "../../css/common/top_area.scss";
 import Logo from "../modules/Logo";
 
+// 폰트어썸 불러오기 ////
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// 돋보기 아이콘 ///
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 export default function TopArea() {
+
+  // [ 검색 관련 함수들 ] ///////
+  // 1. 검색창 보이기함수 ////
+  const showSearch = (e) => {
+    // 기본기능막기
+    e.preventDefault();
+
+  }; //////////// showSearch 함수 //////////
+
+  // 2. 검색창에 엔터키 누르면 검색함수 호출함수
+  const enterKey = () => {};
+
+  // 3. 검색페이지로 검색어와 함께 이동하기 함수
+  const goSearch = () => {};
+
+
+
   /// 리턴 코드구역 ////////
   return (
     <>
@@ -57,6 +79,42 @@ export default function TopArea() {
                 }
               </li>
             ))}
+            {/* 3. 검색, 회원가입, 로그인 메뉴 */}
+            <li
+              style={{
+                marginLeft: "auto",
+                marginRight: "25px",
+              }}
+            >
+              {/* 검색입력박스 */}
+              <div className="searchingGnb">
+                {/* 검색버튼 돋보기 아이콘 */}
+                <FontAwesomeIcon 
+                  icon={faSearch}
+                  className="schbtnGnb"
+                  title="Open search"
+                  onClick={()=>{}}
+                />
+                {/* 입력창 */}
+                <input 
+                  type="text" 
+                  name="schinGnb"
+                  id="schinGnb"
+                  placeholder="Filter by Keyword"
+                  onKeyUp={()=>{}}
+                />
+              </div>
+              {/* 검색기능링크 - 클릭시 검색창 보이기 */}
+              <a href="#" onClick={()=>{}}>
+                <FontAwesomeIcon icon={faSearch} />
+              </a>
+            </li>
+            <li>
+              <Link to="/member">JOIN US</Link>
+            </li>
+            <li>
+              <Link to="/login">LOGIN</Link>
+            </li>
           </ul>
         </nav>
         {/* 모바일용 햄버거 버튼 */}
