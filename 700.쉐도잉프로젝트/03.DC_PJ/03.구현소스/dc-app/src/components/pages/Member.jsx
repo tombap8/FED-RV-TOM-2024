326dab0c80d0a2f38f5de876b383ab6296c1cfaa
Tom B. Parker <tombap8@hotmail.com>
2025-03-07 12:00:25 +0900

    Update Member.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/pages/Member.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/pages/Member.jsx
@@ -3,10 +3,74 @@
 import React from "react";
 
 // 모듈 CSS 불러오기 ///
-import '../../css/pages/member.scss';
+import "../../css/pages/member.scss";
 import { Link } from "react-router-dom";
 
+// 로컬스토리지 생성 JS ////
+import { initData } from "../../js/func/mem_fn";
+
 function Member() {
+  // [ 상태관리변수 ] /////////////
+  // [1] 입력요소 상태변수
+  // 1. 아이디변수
+  const [userId, setUserId] = useState("");
+  // 2. 비밀번호변수
+  const [pwd, setPwd] = useState("");
+  // 3. 비밀번호확인변수
+  const [chkPwd, setChkPwd] = useState("");
+  // 4. 사용자이름변수
+  const [userName, setUserName] = useState("");
+  // 5. 이메일변수
+  const [email, setEmail] = useState("");
+  // 6. 주소변수
+  const [addr, setAddr] = useState("");
+  // 7. 우편번호변수
+  const [zipcode, setZipcode] = useState("");
+
+  // [2] 에러상태관리 변수
+  // -> 에러상태값 초기값은 에러아님(false)
+  // 1. 아이디변수
+  const [userIdError, setUserIdError] = useState(false);
+  // 2. 비밀번호변수
+  const [pwdError, setPwdError] = useState(false);
+  // 3. 비밀번호확인변수
+  const [chkPwdError, setChkPwdError] = useState(false);
+  // 4. 사용자이름변수
+  const [userNameError, setUserNameError] = useState(false);
+  // 5. 이메일변수
+  const [emailError, setEmailError] = useState(false);
+  // 6. 주소변수
+  const [addrError, setAddrError] = useState("");
+
+  // console.log(">>>>", userIdError);
+
+  // [ 아이디관련 메시지 프리셋 ] ////
+  const msgId = [
+    // 1. 최소 5글자 이상 입력할것
+    "User ID must contain a minimum of 5 characters",
+    // 2. 이미 사용중인 아이디임
+    "This ID is already in use!",
+    // 3. 훌륭한 아이디!
+    "That's a great ID!",
+  ];
+
+  // [ 기타 메시지 프리셋 ]
+  const msgEtc = {
+    // 비밀번호
+    pwd: "5 to 15 digits in the form of special characters, characters, and numbers",
+    // 비밀번호확인
+    confPwd: "Password verification does not match",
+    // 필수입력
+    req: "This is a required entry",
+    // 이메일
+    email: "Please enter a valid email format",
+  }; ///// msgEtc ///////
+
+  // [3] 에러메시지 상태변수 : 초기값 msgId[0]
+  // -> 기본 메시지가 출력됨
+  const [idMsg, setIdMsg] = useState(msgId[0]);
+
+  // 리턴 코드구역 ///////////////
   return (
     <div className="outbx">
       <section className="membx">
