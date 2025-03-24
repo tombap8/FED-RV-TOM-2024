5d122dae40386ba87858dadb1b5a55cadad04b51
Tom B. Parker <tombap8@hotmail.com>
2025-03-24 13:26:20 +0900

    aa

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/Weather.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/Weather.jsx
@@ -12,17 +12,41 @@ const Weather = () => {
     city: "",
   });
 
+  // 사용자의 현재 위치를 가져오는 함수
+  const getCurrentLocation = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  };
+
+  // 도시명을 가져오는 함수
+  const getCityName = async (latitude, longitude) => {
+    const apiKey = "7fdf8fb74f3e2ed02bfb7e298a32df49";
+    const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    const response = await axios.get(url);
+    return response.data[0].name;
+  };
+
   // 컴포넌트가 마운트 되었을 때 실행되는 useEffect
   useEffect(() => {
-    const cityName = "Seoul"; // 도시명 설정
-    const apiKey = "7fdf8fb74f3e2ed02bfb7e298a32df49"; // API 키
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`; // 날씨 정보 조회 URL
+    const fetchWeatherData = async () => {
+      try {
+        // 현재 위치 가져오기
+        const position = await getCurrentLocation();
+        const { latitude, longitude } = position.coords;
 
-    // axios를 이용한 데이터 조회
-    axios
-      .get(url)
-      .then((result) => {
-        const wdata = result.data; // 데이터 파싱
+        // 도시명 가져오기
+        const cityName = await getCityName(latitude, longitude);
+
+        console.log(cityName);
+        // 날씨 정보 조회 URL
+        const apiKey = "7fdf8fb74f3e2ed02bfb7e298a32df49";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+        // axios를 이용한 데이터 조회
+        const result = await axios.get(url);
+        const wdata = result.data;
+
         // 상태 변수 업데이트
         setWeatherData({
           temp: wdata.main.temp,
@@ -31,9 +55,13 @@ const Weather = () => {
           loading: false,
           city: cityName,
         });
-      })
-      .catch((err) => console.log(err)); // 에러 처리
-  }, []); // 빈 배열을 두어 컴포넌트가 처음 렌더링될 때만 실행
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchWeatherData();
+  }, []);
 
   const isrc = `https://openweathermap.com/img/w/${weatherData.icon}.png`; // 날씨 아이콘 URL
 
