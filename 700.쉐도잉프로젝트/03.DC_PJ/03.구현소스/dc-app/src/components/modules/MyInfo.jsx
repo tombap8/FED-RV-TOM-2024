777c8716089fcaef7087ac865ac2c5a952516dff
Tom B. Parker <tombap8@hotmail.com>
2025-04-04 15:00:28 +0900

    Update MyInfo.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/MyInfo.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/modules/MyInfo.jsx
@@ -28,13 +28,13 @@ function MyInfo() {
   // [ 상태관리변수 ] /////////////
   // [1] 입력요소 상태변수
   // 1. 아이디변수
-  const [userId, setUserId] = useState("");
+//   const [userId, setUserId] = useState("");
   // 2. 비밀번호변수
   const [pwd, setPwd] = useState("");
   // 3. 비밀번호확인변수
   const [chkPwd, setChkPwd] = useState("");
   // 4. 사용자이름변수
-  const [userName, setUserName] = useState("");
+//   const [userName, setUserName] = useState("");
   // 5. 이메일변수
   const [email, setEmail] = useState("");
   // 6. 주소변수
@@ -45,13 +45,13 @@ function MyInfo() {
   // [2] 에러상태관리 변수
   // -> 에러상태값 초기값은 에러아님(false)
   // 1. 아이디변수
-  const [userIdError, setUserIdError] = useState(false);
+//   const [userIdError, setUserIdError] = useState(false);
   // 2. 비밀번호변수
   const [pwdError, setPwdError] = useState(false);
   // 3. 비밀번호확인변수
   const [chkPwdError, setChkPwdError] = useState(false);
   // 4. 사용자이름변수
-  const [userNameError, setUserNameError] = useState(false);
+//   const [userNameError, setUserNameError] = useState(false);
   // 5. 이메일변수
   const [emailError, setEmailError] = useState(false);
   // 6. 주소변수
@@ -87,84 +87,84 @@ function MyInfo() {
 
   // [ 유효성 검사 함수 ] ///////
   // 1. 아이디 유효성 검사 ////////////
-  const changeUserId = (e) => {
-    // 입력된 값읽기
-    let val = e.target.value;
+//   const changeUserId = (e) => {
+//     // 입력된 값읽기
+//     let val = e.target.value;
 
-    // 1. 아이디 유효성 검사식(따옴표로 싸지 말것!)
-    const valid = /^[A-Za-z0-9+]{5,}$/;
-    // 유효성 검사방법: 정규식.test(값)
+//     // 1. 아이디 유효성 검사식(따옴표로 싸지 말것!)
+//     const valid = /^[A-Za-z0-9+]{5,}$/;
+//     // 유효성 검사방법: 정규식.test(값)
 
-    // 2. 입력값 확인 : e.target
-    // console.log(val);
+//     // 2. 입력값 확인 : e.target
+//     // console.log(val);
 
-    // 3. 에러상태 분기하기
-    // 3-1. 에러 아닐때 (유효성검사만 통과한 경우)
-    if (valid.test(val)) {
-      console.log("통과했지만...!");
-      // 아이디 검사를 위해 기본 데이터 생성호출!
-      initData();
-      // 로컬스토리지에 "mem-data"가 없으면 초기셋팅함!
+//     // 3. 에러상태 분기하기
+//     // 3-1. 에러 아닐때 (유효성검사만 통과한 경우)
+//     if (valid.test(val)) {
+//       console.log("통과했지만...!");
+//       // 아이디 검사를 위해 기본 데이터 생성호출!
+//       initData();
+//       // 로컬스토리지에 "mem-data"가 없으면 초기셋팅함!
 
-      // 이제 중복 아이디 검사를 실행한다!!!
-      // 1. 로컬스 변수할당
-      let memData = localStorage.getItem("mem-data");
-      console.log(memData);
+//       // 이제 중복 아이디 검사를 실행한다!!!
+//       // 1. 로컬스 변수할당
+//       let memData = localStorage.getItem("mem-data");
+//       console.log(memData);
 
-      // 2. 로컬스 객체변환 (왜? 문자형이니까!)
-      memData = JSON.parse(memData);
-      console.log(memData);
-      // -> 배열데이터로 변환!
-      // 주의: JSON 파싱할때 원본형식이 제이슨 파일형식으로
-      // 엄격하게 작성되어야 에러가 없음(마지막콤마 불허용 등)
+//       // 2. 로컬스 객체변환 (왜? 문자형이니까!)
+//       memData = JSON.parse(memData);
+//       console.log(memData);
+//       // -> 배열데이터로 변환!
+//       // 주의: JSON 파싱할때 원본형식이 제이슨 파일형식으로
+//       // 엄격하게 작성되어야 에러가 없음(마지막콤마 불허용 등)
 
-      // 3. 배열이니까 현재 입력데이터의 아이디가
-      // 기존 배열값으로 있는지 검사함!
-      // 있으면 true, 없으면 false
-      let isT = memData.some((v) => v.uid === val);
-      console.log("중복id있어?", isT);
+//       // 3. 배열이니까 현재 입력데이터의 아이디가
+//       // 기존 배열값으로 있는지 검사함!
+//       // 있으면 true, 없으면 false
+//       let isT = memData.some((v) => v.uid === val);
+//       console.log("중복id있어?", isT);
 
-      // 4. true 일 경우 중복데이터 메시지 표시
-      if (isT) {
-        // 에러 메시지 업데이트
-        setIdMsg(msgId[1]);
-        // 에러상태값 업데이트
-        setUserIdError(true);
-      } ///// if /////
-      // 5. false 일 경우 [성공 메시지] 표시
-      else {
-        // 에러상태값 업데이트 : 에러가 아님!(false)
-        setUserIdError(false);
-      } ///// else //////
+//       // 4. true 일 경우 중복데이터 메시지 표시
+//       if (isT) {
+//         // 에러 메시지 업데이트
+//         setIdMsg(msgId[1]);
+//         // 에러상태값 업데이트
+//         setUserIdError(true);
+//       } ///// if /////
+//       // 5. false 일 경우 [성공 메시지] 표시
+//       else {
+//         // 에러상태값 업데이트 : 에러가 아님!(false)
+//         setUserIdError(false);
+//       } ///// else //////
 
-      // [ 새로운 배열메서드 : some() ]
-      // -> 조건에 맞는 값이 하나만 나오면 true처리함
-      // 비교참고) every() 는 하나만 false이면 false리턴
-      // let isT = memData.some(v=>{
-      //     console.log("돌아!",v.uid);
-      //     return v.uid===val;
-      // });
-      // let isT = memData.every(v=>{
-      //     console.log("돌아!",v.uid);
-      //     return v.uid===val;
-      // });
+//       // [ 새로운 배열메서드 : some() ]
+//       // -> 조건에 맞는 값이 하나만 나오면 true처리함
+//       // 비교참고) every() 는 하나만 false이면 false리턴
+//       // let isT = memData.some(v=>{
+//       //     console.log("돌아!",v.uid);
+//       //     return v.uid===val;
+//       // });
+//       // let isT = memData.every(v=>{
+//       //     console.log("돌아!",v.uid);
+//       //     return v.uid===val;
+//       // });
 
-      // 아이디 에러상태 업데이트(false)
-      //   setUserIdError(false);
-    } /// if /////////////////////////
-    // 3-2. 에러일때 : 유효성 검사 에러
-    else {
-      console.log("에러~!");
-      // 에러 메시지 업데이트
-      setIdMsg(msgId[0]);
-      // 아이디 에러상태 업데이트(true)
-      setUserIdError(true);
-    } /// else ///
+//       // 아이디 에러상태 업데이트(false)
+//       //   setUserIdError(false);
+//     } /// if /////////////////////////
+//     // 3-2. 에러일때 : 유효성 검사 에러
+//     else {
+//       console.log("에러~!");
+//       // 에러 메시지 업데이트
+//       setIdMsg(msgId[0]);
+//       // 아이디 에러상태 업데이트(true)
+//       setUserIdError(true);
+//     } /// else ///
 
-    // 실제 userId 상태변수값이 업데이트 돼야만
-    // 화면에 출력된다!
-    setUserId(val);
-  }; ////////// changeUserId 함수 ////////////
+//     // 실제 userId 상태변수값이 업데이트 돼야만
+//     // 화면에 출력된다!
+//     setUserId(val);
+//   }; ////////// changeUserId 함수 ////////////
 
   // 2. 비밀번호 유효성 검사 ///////////
   const changePwd = (e) => {
@@ -199,17 +199,17 @@ function MyInfo() {
   }; ///////// changeChkPwd 함수 //////////
 
   // 4. 사용자이름 유효성 검사 ///////////
-  const changeUserName = (e) => {
-    // 입력된 값읽기
-    let val = e.target.value;
+//   const changeUserName = (e) => {
+//     // 입력된 값읽기
+//     let val = e.target.value;
 
-    // 1. 빈값체크
-    if (val !== "") setUserNameError(false);
-    else setUserNameError(true);
+//     // 1. 빈값체크
+//     if (val !== "") setUserNameError(false);
+//     else setUserNameError(true);
 
-    // 2. 기존입력값 반영하기
-    setUserName(val);
-  }; ///////// changeUserName 함수 //////////
+//     // 2. 기존입력값 반영하기
+//     setUserName(val);
+//   }; ///////// changeUserName 함수 //////////
 
   // 5. 이메일 유효성 검사 ///////////
   const changeEmail = (e) => {
@@ -252,15 +252,15 @@ function MyInfo() {
     // (2) 우편번호 저장
     setZipcode(zc);
     console.log(zipcode);
-  }; ///////// changeUserName 함수 //////////
+  }; ///////// changeAddr 함수 //////////
 
   // [ 전체 유효성검사 체크함수 ] ///////////
   const totalValid = () => {
     // 1. 모든 상태변수에 빈값일때 에러상태값 업데이트!
-    if (!userId) setUserIdError(true);
+    // if (!userId) setUserIdError(true);
     if (!pwd) setPwdError(true);
     if (!chkPwd) setChkPwdError(true);
-    if (!userName) setUserNameError(true);
+    // if (!userName) setUserNameError(true);
     if (!email) setEmailError(true);
     // 주소체크 추가
     if (!addr) setAddrError(true);
@@ -271,15 +271,15 @@ function MyInfo() {
     // 2. 통과시 true, 불통과시 false 리턴처리
     // 통과조건 : 빈값아님 + 에러후크변수가 모두 false
     if (
-      userId &&
+    //   userId &&
       pwd &&
       chkPwd &&
-      userName &&
+    //   userName &&
       email &&
-      !userIdError &&
+    //   !userIdError &&
       !pwdError &&
       !chkPwdError &&
-      !userNameError &&
+    //   !userNameError &&
       !emailError &&
       // 주소에러항목추가
       !addrError
@@ -309,39 +309,27 @@ function MyInfo() {
 
       // 3. 로컬스 객체변환
       memData = JSON.parse(memData);
-      // 최대수를 위한 배열값 뽑기 (idx항목)
-      let temp = memData.map((v) => v.idx);
-      // 다음 번호는 항상 최대수+1이다!
-      console.log("다음번호:", Math.max(...temp) + 1);
 
-      // 4. 새로운 데이터 구성하기
-      let newData = {
-        idx: Math.max(...temp) + 1,
-        uid: userId,
-        pwd: pwd,
-        unm: userName,
-        eml: email,
-        // 추가항목1 : 우편번호
-        zcode: zipcode,
-        // 추가항목2 : 주소
-        addr: addr,
-      };
+      // 4. 로컬스에서 해당 사용자 정보 업데이트 하기!
+      memData.find(v=>{
+        if(v.uid === userId){
+            // 비밀번호 변경
+          v.pwd = pwd;
+          // 이메일 변경
+          v.eml = email;
+          // 우편번호 변경
+          v.zcode = zipcode;
+          // 주소 변경
+          v.addr = addr;
+        } /// if ///
+      }); /// find ///           
 
-      // 5. 데이터 추가하기 : 배열에 데이터 추가 push()
-      memData.push(newData);
-
-      // 6. 로컬스에 반영하기 : 문자화해서 넣어야함!
+      // 5. 로컬스에 반영하기 : 문자화해서 넣어야함!
       localStorage.setItem("mem-data", JSON.stringify(memData));
 
-      // 7. 회원가입 환영메시지 + 로그인 페이지 이동
-      // 버튼 텍스트에 환영메시지
-      document.querySelector(".sbtn").innerText = "Thank you for joining us!";
-      // 1초후 페이지 이동 : 라우터 Navigate로 이동함
-      setTimeout(() => {
-        goPage("/login");
-        // 주의: 경로앞에 슬래쉬(/) 안쓰면
-        // 현재 Memeber 경로 하위 경로를 불러옴
-      }, 1000);
+      // 6. 반영후 메시지 띄우기
+      alert('Your information has been successfully updated according to the new mode.');
+      
     } ///////// if /////////
     // 3. 불통과시 /////
     else {
@@ -368,40 +356,6 @@ function MyInfo() {
                 readOnly={true}
                 defaultValue={loginInfo.uid}
               />
-              {
-                // 에러일 경우 메시지 출력
-                // 조건문 && 출력요소
-                userIdError && (
-                  <div className="msg">
-                    <small
-                      style={{
-                        color: "red",
-                        fontSize: "10px",
-                      }}
-                    >
-                      {idMsg}
-                    </small>
-                  </div>
-                )
-              }
-              {
-                // 통과시 메시지 출력
-                // 조건문 && 출력요소
-                // 조건추가 : userId가 입력전일때 안보임처리
-                // userId가 입력전엔 false로 리턴됨!
-                !userIdError && userId && (
-                  <div className="msg">
-                    <small
-                      style={{
-                        color: "green",
-                        fontSize: "10px",
-                      }}
-                    >
-                      {msgId[2]}
-                    </small>
-                  </div>
-                )
-              }
             </li>
             <li>
               <label>Password : </label>
@@ -466,22 +420,6 @@ function MyInfo() {
                 readOnly={true}
                 defaultValue={loginInfo.unm}
               />
-              {
-                // 에러일 경우 메시지 출력
-                // 조건문 && 출력요소
-                userNameError && (
-                  <div className="msg">
-                    <small
-                      style={{
-                        color: "red",
-                        fontSize: "10px",
-                      }}
-                    >
-                      {msgEtc.req}
-                    </small>
-                  </div>
-                )
-              }
             </li>
             <li>
               <label>Address</label>
@@ -537,12 +475,8 @@ function MyInfo() {
             </li>
             <li style={{ overflow: "hidden" }}>
               <button className="sbtn" onClick={onSubmit}>
-                Submit
+                Update
               </button>
-            </li>
-            <li>
-              Are you already a Member?
-              <Link to="/login">Log In</Link>
             </li>
           </ul>
         </form>
