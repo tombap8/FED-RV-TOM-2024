3a89be6d843d81047d3054b8f19a7eb023e0f90b
Tom B. Parker <tombap8@hotmail.com>
2025-04-23 11:54:22 +0900

    ss

---
+++ 9995.Firebase/firebase-app/src/components/UserFormList.jsx
@@ -0,0 +1,24 @@
+// 사용자 정보 입력 및 리스트 출력 컴포넌트
+// Firebase와 연결된 사용자 정보 입력 및 리스트 출력 컴포넌트
+
+const UserFormList = () => {
+  return (
+    <div>
+      <h2>Firebase Users</h2>
+      {/* 이름 입력 필드 */}
+        <input 
+        type="text" 
+        placeholder="이름을 입력하세요"
+        />
+        {/* 나이 입력 필드 */}
+        <input
+        type="number"
+        placeholder="나이을 입력해주세요"
+        />
+        {/* 사용자 추가 버튼 */}
+        <button>사용자 추가</button>
+    </div>
+  );
+};
+
+export default UserFormList;
--- 9995.Firebase/firebase-app/src/index.js
+++ 9995.Firebase/firebase-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 // 메인 페이지용 CSS
 import './css/main.scss';
-
+import UserFormList from './components/UserFormList';
 
 
 // 루트요소 선택하기 ///
@@ -15,8 +15,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <>
     <h1>
-      <b>선재업고</b>
-      <span>튀어</span>
+      <b>리액트 + </b>
+      <span>파이어베이스</span>
     </h1>
+    {/* 사용자 정보 입력 및 리스트 컴포넌트 */}
+    <UserFormList />
   </>
 );
